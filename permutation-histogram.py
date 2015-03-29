5810e4b231ecf3f76fd2c188cb9ba23bf76fda72
Simon Crase <simon@greenweaves.nz>
2015-03-30 11:17:33 +1300

    Started code to generate histogram

---
+++ permutation-histogram.py
@@ -0,0 +1,32 @@
+# permutation-test.py
+
+# Copyright (C) 2015 Greenweaves Software Oty Ltd
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+import permutation
+import math
+
+perms={}
+     
+for i in range(120000):
+    perm0=permutation.ran_perm(5)
+    perm = permutation.permutation2str(perm0)
+    if perm in perms:
+        perms[perm]+=1
+    else:
+        perms[perm]=1
+
+for kk in perms.keys():
+    print kk,", ",perms[kk]
