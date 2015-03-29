9674a45ee596ce4bdf6f090c2672f8936a3ab0e0
Simon Crase <simon@greenweaves.nz>
2015-03-30 10:01:13 +1300

    Fixed error in standard deviation

--- permutation-test.py
+++ permutation-test.py
@@ -1,3 +1,20 @@
+# permutation-test.py
+
+# Copyright (C) 2015 Greenweaves Software Oty Ltd
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
 import permutation
 import math
 
@@ -10,7 +27,7 @@ def per_to_str(perm):
     
 perms={}
     
-for i in range(120000):
+for i in range(1200000):
     perm0=permutation.ran_perm(5)
     perm = per_to_str(perm0)
     if perm in perms:
@@ -18,21 +35,20 @@ for i in range(120000):
     else:
         perms[perm]=1
     
-kk=perms.keys()
-kk.sort()
-print len(kk)
+count= len(perms.keys())
+print count
 
 sum=0
-for kkk in kk:
-    sum+=perms[kkk]
+for kk in perms.keys():
+    sum+=perms[kk]
 
-average=sum/len(kk)
+average=sum/count
 
 var=0
-for kkk in kk:
-    var+=((perms[kkk]-average)*(perms[kkk]-average))
+for kk in perms.keys():
+    var+=((perms[kk]-average)*(perms[kk]-average))
 
 
-sigma=math.sqrt(var/len(kkk))
+sigma=math.sqrt(var/count)
 
 print average, sigma
