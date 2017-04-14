bfc15b3d35843ae375d28e135efe90e255c1a2b1
Simon Crase <simon@greenweaves.nz>
2017-04-14 19:33:57 +1200

    Implemneted Gauss cut

--- homework-6-a1.py
+++ homework-6-a1.py
@@ -1,14 +1,20 @@
 import random, math, pylab
 
+def gauss_cut(cut=1.0):
+    while True:
+        x = random.gauss(0.0, 1.0)
+        if abs(x) <= cut:
+            return x
+        
 alpha = 0.5
 nsamples = 1000000
 samples_x = []
 samples_y = []
 for sample in range(nsamples):
     while True:
-        x = random.uniform(-1.0, 1.0)
-        y = random.uniform(-1.0, 1.0)
-        p = math.exp(-0.5 * (x ** 2 + y ** 2) - alpha * (x ** 4 + y ** 4))
+        x = gauss_cut()
+        y = gauss_cut()
+        p = math.exp( - alpha * (x ** 4 + y ** 4))
         if random.uniform(0.0, 1.0) < p:
             break
     samples_x.append(x)
