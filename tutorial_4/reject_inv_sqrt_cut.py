240f9d774a084d45d4c01af43d2695892bfc637e
Simon Crase <simon@greenweaves.nz>
2017-04-15 12:23:26 +1200

    Converted to Python 3

--- tutorial_4/reject_inv_sqrt_cut.py
+++ tutorial_4/reject_inv_sqrt_cut.py
@@ -2,7 +2,7 @@ import random, math, pylab
 
 y_max = 100.0
 x_cut = 1.0
-n_data = 10000
+n_data = 100000
 data = []
 n_accept = 0
 while n_accept < n_data: 
@@ -13,7 +13,7 @@ while n_accept < n_data:
         data.append(x)
 
 pylab.hist(data, bins=100, normed='True')
-x = [a / 100.0 for a in xrange(1, 100)]
+x = [a / 100.0 for a in range(1, 100)]
 y = [1.0 / (2.0 * math.sqrt(a)) for a in x]
 pylab.plot(x, y, 'red', linewidth = 2)
 pylab.title('Theoretical distribution $\pi(x)={1}/{(2 \sqrt{x})}$ and normalized\
