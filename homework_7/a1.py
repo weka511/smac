3c09ed01fc8d03a473631a6a1abd8f40da08a803
Simon Crase <simon@greenweaves.nz>
2017-04-18 17:28:58 +1200

    Started Homework 7 A1

---
+++ homework_7/a1.py
@@ -0,0 +1,19 @@
+import math, random, pylab
+
+def levy_harmonic_path(k):
+    x = [random.gauss(0.0, 1.0 / math.sqrt(2.0 * math.tanh(k * beta / 2.0)))]
+    if k == 2:
+        Ups1 = 2.0 / math.tanh(beta)
+        Ups2 = 2.0 * x[0] / math.sinh(beta)
+        x.append(random.gauss(Ups2 / Ups1, 1.0 / math.sqrt(Ups1)))
+    return x[:]
+
+beta = 2.0
+nsteps = 1000000
+low = levy_harmonic_path(2)
+high = low[:]
+data = []
+for step in range(nsteps):
+    k = random.choice([0, 1])
+    low[k] = levy_harmonic_path(1)[0]
+    high[k] = low[k]
