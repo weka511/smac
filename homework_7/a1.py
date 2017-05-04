53db71ce1331a66dce86f4a428c338ffb7156a7e
Simon Crase <simon@greenweaves.nz>
2017-05-04 17:11:06 +1200

    Added Chi-square test fo permutation_sample.py

--- homework_7/a1.py
+++ homework_7/a1.py
@@ -1,5 +1,9 @@
 import math, random, pylab
 
+def pi_x(x, beta):
+    sigma = 1.0 / math.sqrt(2.0 * math.tanh(beta / 2.0))
+    return math.exp(-x ** 2 / (2.0 * sigma ** 2)) / math.sqrt(2.0 * math.pi) / sigma
+
 def levy_harmonic_path(k):
     x = [random.gauss(0.0, 1.0 / math.sqrt(2.0 * math.tanh(k * beta / 2.0)))]
     if k == 2:
--- lecture_7/permutation_sample.py
+++ lecture_7/permutation_sample.py
@@ -1,9 +1,12 @@
-import random
+import random,operator,functools,scipy.stats
 
-N = 3
+N = 5
 statistics = {}
 L = [l for l in range(N)]
-nsteps = 10000
+nsteps = 10000000
+k=functools.reduce(operator.mul, list(range(1,N+1)),1)
+expected = nsteps/k
+
 for step in range(nsteps):
     i = random.randint(0, N - 1)
     j = random.randint(0, N - 1)
@@ -12,9 +15,11 @@ for step in range(nsteps):
         statistics[tuple(L)] += 1
     else:
         statistics[tuple(L)] = 1
-    print (L)
-    print ([l for l in range(N)])
-    print ()
 
 for item in statistics:
     print (item, statistics[item])
+
+f_obs = [statistics[item] for item in statistics]
+
+s = scipy.stats.chisquare(f_obs)
+print ('M={}, k={}, nsteps={}, Chi square={:.1f}, p-value={:.2g}'.format(N,k, nsteps, s.statistic, s.pvalue))
