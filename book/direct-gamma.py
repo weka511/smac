8b04ed95ae4214ae42f795c37dc178a2146bcee0
Simon Crase <simon@greenweaves.nz>
2017-05-16 11:40:35 +1200

    Generate histogram

--- book/direct-gamma.py
+++ book/direct-gamma.py
@@ -1,30 +1,31 @@
-import random,pylab,math
+import random,pylab,math,numpy as np
 
 def direct_gamma(gamma,generator=False,K=10000,N=1000000):
     sigma = 0
-    sigma_sq = 0
     for i in range(N):
         x = random.uniform(0,1)
         sigma += x**gamma
-        sigma_sq += x**(2*gamma)
-        if generator and i%K==0:
-            yield(i,sigma/(i+1))        
-    diff = sigma_sq-sigma*sigma
-    Obs = sigma/N
-    return (Obs,math.sqrt(sigma_sq/N-Obs*Obs)/N)
+    return sigma/N
 
 
 if __name__ == '__main__':
-    pylab.style.use('ggplot')
-    xs = []
-    ys = []
+    M = 1000
+    gamma = -0.8
     
-    for i,Obs in direct_gamma(-0.8,generator=True):
-        xs.append(i)
-        ys.append(Obs)
-    pylab.plot(xs,ys)
-    pylab.xlabel(r'$Number\ of\ samples\ i$')
-    pylab.ylabel(r'$Running\ average\ \frac{\Sigma}{N}$')
-    pylab.title(r'$Value\ of\ \gamma\ integral$')
-    pylab.savefig('direct-gamma.png')
+    for N in [1,10,100,1000,10000]:
+        def scaled(n):
+            return (direct_gamma(gamma,N=n)-5)/(N**(-1-gamma))
+        #data = [scaled(n+1) for n in range(N)]
+        data = [direct_gamma(gamma,N=N) for m in range(M)]
+        y,binEdges=np.histogram(data,normed=True)
+        bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
+        pylab.plot(bincenters,y,'-',label='N={0}'.format(N))
+        
+        #pylab.xlim(1,10)
+    pylab.xlabel(r'$\Sigma/N$')
+    pylab.ylabel(r'$\pi(\Sigma/N$)')
+    pylab.legend()
+    pylab.savefig('histogram.png')
     pylab.show()
+    
+
