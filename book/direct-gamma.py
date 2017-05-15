ef61dd671d739d24206c3ed27676244d7039b250
Simon Crase <simon@greenweaves.nz>
2017-05-16 10:00:34 +1200

    Produce plot similar to 1.43

--- book/direct-gamma.py
+++ book/direct-gamma.py
@@ -1,20 +1,29 @@
 import random,pylab,math
 
-def direct_gamma(N,gamma):
+def direct_gamma(gamma,generator=False,K=10000,N=1000000):
     sigma = 0
     sigma_sq = 0
     for i in range(N):
         x = random.uniform(0,1)
         sigma += x**gamma
         sigma_sq += x**(2*gamma)
+        if generator and i%K==0:
+            yield(i,sigma/(i+1))        
     diff = sigma_sq-sigma*sigma
     Obs = sigma/N
-    #print (diff)
     return (Obs,math.sqrt(sigma_sq/N-Obs*Obs)/N)
 
-N=10000
 
 if __name__ == '__main__':
-    for gamma in [2,1,0,-0.2,-0.4,-0.8]:
-        Obs,Error = direct_gamma(N,gamma)
-        print (gamma, Obs,Error,1/(1+gamma))
+    xs = []
+    ys = []
+    
+    for i,Obs in direct_gamma(-0.8,generator=True):
+        xs.append(i)
+        ys.append(Obs)
+    pylab.plot(xs,ys)
+    pylab.xlabel(r'$Number\ of\ samples\ i$')
+    pylab.ylabel(r'$Running\ average\ \frac{\Sigma}{N}$')
+    pylab.title(r'$Value\ of\ \gamma\ integral$')
+    pylab.savefig('direct-gamma.png')
+    pylab.show()
