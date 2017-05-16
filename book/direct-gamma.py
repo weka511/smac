33bd1d52a66fc5d2c8da360b6ec0ed6b02673c2f
Simon Crase <simon@greenweaves.nz>
2017-05-16 14:50:33 +1200

    Generate histogram - not matching textbook very well

--- book/direct-gamma.py
+++ book/direct-gamma.py
@@ -3,27 +3,28 @@ import random,pylab,math,numpy as np
 def direct_gamma(gamma,generator=False,K=10000,N=1000000):
     sigma = 0
     for i in range(N):
-        x = random.uniform(0,1)
+        x = random.random()
         sigma += x**gamma
     return sigma/N
 
 
 if __name__ == '__main__':
-    M = 1000
+    M = 100000
     gamma = -0.8
     
     for N in [1,10,100,1000,10000]:
         def scaled(n):
             return (direct_gamma(gamma,N=n)-5)/(N**(-1-gamma))
-        #data = [scaled(n+1) for n in range(N)]
-        data = [direct_gamma(gamma,N=N) for m in range(M)]
-        y,binEdges=np.histogram(data,normed=True)
+        data = [scaled(N) for m in range(M)]
+        #data = [direct_gamma(gamma,N=N) for m in range(M)]
+        y,binEdges=np.histogram(data,normed=True,bins=int(M/50))
         bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
-        pylab.plot(bincenters,y,'-',label='N={0}'.format(N))
-        
-        #pylab.xlim(1,10)
+        pylab.plot(bincenters,y,'-',label='N={0}'.format(N))       
+    #pylab.xlim(1,10)
+    pylab.xlim(-10,2)
     pylab.xlabel(r'$\Sigma/N$')
     pylab.ylabel(r'$\pi(\Sigma/N$)')
+    pylab.title(r'$Average\ {0}\ iterations$'.format(M))
     pylab.legend()
     pylab.savefig('histogram.png')
     pylab.show()
