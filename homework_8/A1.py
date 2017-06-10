f026190a24f2f29b8dc89e892a8732f3a75a0936
Simon Crase <simon@greenweaves.nz>
2017-06-11 09:10:17 +1200

    Started on Homework 8

---
+++ homework_8/A1.py
@@ -0,0 +1,28 @@
+import random, math
+
+def energy(S, N, nbr):
+    E = 0.0
+    for k in range(N):
+        E -=  S[k] * sum(S[nn] for nn in nbr[k])
+    return 0.5 * E
+
+L = 6
+N = L * L
+nbr = {i : ((i // L) * L + (i + 1) % L, (i + L) % N,
+            (i // L) * L + (i - 1) % L, (i - L) % N) \
+                                    for i in range(N)}
+
+T = 2.0
+S = [random.choice([1, -1]) for k in range(N)]
+nsteps = N * 100
+beta = 1.0 / T
+Energy = energy(S, N, nbr)
+E = []
+for step in range(nsteps):
+    k = random.randint(0, N - 1)
+    delta_E = 2.0 * S[k] * sum(S[nn] for nn in nbr[k])
+    if random.uniform(0.0, 1.0) < math.exp(-beta * delta_E):
+        S[k] *= -1
+        Energy += delta_E
+    E.append(Energy)
+print ('mean energy per spin: {0}'.format(sum(E) / float(len(E) * N)))
