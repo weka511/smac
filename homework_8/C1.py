7bdff7c820a6b85e0cbcede8ec2371fe922bc942
Simon Crase <simon@greenweaves.nz>
2017-06-13 09:37:14 +1200

    Accumulate sum rather than list to save time

--- homework_8/C1.py
+++ homework_8/C1.py
@@ -5,13 +5,13 @@ N = L * L
 nbr = {i : ((i // L) * L + (i + 1) % L, (i + L) % N,
             (i // L) * L + (i - 1) % L, (i - L) % N) \
                                     for i in range(N)}
-nsteps = 1000000 * N
+nsteps = 10000000 * N
 T = 2.0
 beta = 1.0 / T
 S = [random.choice([-1, 1]) for site in range(N)]
 E = -0.5 * sum(S[k] * sum(S[nn] for nn in nbr[k]) \
                                 for k in range(N))
-Energies = []
+TotalEnergy = 0
 for step in range(nsteps):
     k = random.randint(0, N - 1)
     Upsilon = random.uniform(0.0, 1.0)
@@ -22,6 +22,12 @@ for step in range(nsteps):
         S[k] = 1
     if S[k] != Sk_old:
         E -= 2.0 * h * S[k]
-    Energies.append(E)
-    
-print ('nsteps = {0}, L={1}, mean energy per spin: {2}'.format(nsteps, L, sum(Energies) / float(len(Energies) * N)))
+    TotalEnergy+=E
+
+mean_energy_per_spin = TotalEnergy / float(nsteps * N)
+
+print (
+    'nsteps = {0:,d}, L={1}, mean energy per spin: {2}'.format(nsteps,
+                                                               L,
+                                                               mean_energy_per_spin)
+)
