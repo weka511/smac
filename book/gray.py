174c0794e0448d3835901f529fab35b335e36fe3
Simon Crase <simon@greenweaves.nz>
2019-04-30 12:04:25 +1200

    Added new class for Gray iterator

--- book/gray.py
+++ book/gray.py
@@ -35,17 +35,55 @@ def gray(N):
         tau[k]   = k+1
         if (k != 1): tau[0] = 1
         yield k
+
+# Gray
+#
+# Class for Gray code. Permits restarting
+
+class Gray:
+    def __init__(self,N,tau=None,i=0):
+        self.N   = N
+        self.tau = list(range(1,(N+1)+1)) if tau==None else tau
+        self.i   = i
+        self.max = 2**(N-1)-1-1
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.i<self.max:
+            k = self.tau[0]
+            if k>self.N: raise StopIteration
+            self.tau[k-1] = self.tau[k]
+            self.tau[k]   = k+1
+            if (k != 1): self.tau[0] = 1
+            return k            
+        else:
+            raise StopIteration
+   
+    def __str__(self):
+        return '['+'.'.join(str(t) for t in self.tau)+']'
+            
         
 if __name__=='__main__':
     import unittest
     
     class GrayTest(unittest.TestCase):
-        def testGray(self):
+        def setUp(self):
             flips    = [1,2,1,3,1,2,1]
-            expected = flips+[1+max(flips)]+flips[::-1]
+            self.expected = flips+[1+max(flips)]+flips[::-1]
+            
+        def testGrayGenerator(self):
             i        = 0
-            for k in gray(5):
-                self.assertEqual(expected[i],k)
+            for k in gray(4):
+                self.assertEqual(self.expected[i],k)
+                i+=1
+                
+        def testGrayIterator(self):
+            i        = 0
+            grey     = Gray(4)
+            for k in grey:
+                self.assertEqual(self.expected[i],k)
                 i+=1
                 
     unittest.main()
