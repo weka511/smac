23a3293be89163fa5433250ddbfa690a1d30f17f
Simon Crase <simon@greenweaves.nz>
2020-01-25 14:51:48 +1300

    Verify orthunormality

--- book/README.md
+++ book/README.md
@@ -35,7 +35,7 @@ Code exercises from the textbook
 ||directDisksAny.py||
 |3||Density Matrices and Path Integrals|
 |3.1.1||The Quantum harmonic oscillator|
-||harmonic_wavefunction.py|Exercise 3.1|
+||harmonic_wavefunction.py|Exercise 3.1: Verify orthonormality of the solutions to Schroedinger's equation for Simple Harmonic Oscillator|
 |4|| Bosons|
 |5|| Order and disorder in spin systems|
 |5.1||The Ising model - exact computations|
--- book/harmonic_wavefunction.py
+++ book/harmonic_wavefunction.py
@@ -1,34 +1,25 @@
-# Plot solutions to Schroedinger's equation for Simple Harmonic Oscillator
+# Verify orthonormality of the solutions to Schroedinger's equation for Simple Harmonic Oscillator
 import sys
 sys.path.append('../')
 import math,  matplotlib.pyplot as plt
 
 n_states = 17
-n_steps  = 100
-L        = 7.0
-step     = L/n_steps
-
+n_steps  = 10000
+L        = 500
+step     = L / n_steps
+omega    = 1
+m        = 1
 grid_x   = [i * step for i in range(-n_steps, n_steps+1)]
 psi      = {}
 
 for x in grid_x:
-    psi[x] = [math.exp(-x ** 2 / 2.0) / math.pi ** 0.25]  # ground state
+    psi[x] = [math.exp(-(m * omega**2 /2 )*x ** 2 ) / math.pi ** 0.25]  # ground state
     psi[x].append(math.sqrt(2.0) * x * psi[x][0])         # first excited state
     # other excited states (through recursion):
     for n in range(2, n_states):
         psi[x].append(math.sqrt(2.0 / n) * x * psi[x][n - 1] -
                       math.sqrt((n - 1.0) / n) * psi[x][n - 2])
-for n in range(n_states):
-    print ('level %i:' % n, [psi[x][n] for x in grid_x])    
-    g,=plt.plot(grid_x,[psi[x][n] for x in grid_x],label=str(n))
 
-print (list(sum(psi[x][i]*psi[x][i] for x in grid_x)/(2*2*L) for i in range(n_states)))
-print (list(sum(psi[x][i]*psi[x][j] for x in grid_x)/(2*2*L) for i in range(n_states) for j in range(i)))
+print (list(step*sum(psi[x][i]*psi[x][i] for x in grid_x) for i in range(n_states)))
+print (list(sum(psi[x][i]*psi[x][j] for x in grid_x) for i in range(n_states) for j in range(i)))
 
-plt.grid()    
-plt.legend([str(n)for n in range(n_states) ])
-plt.xlabel('x')
-plt.ylabel(r'$\psi$')
-plt.title('Harmonic wave function')
-plt.savefig('harmonic.png')
-plt.show()
