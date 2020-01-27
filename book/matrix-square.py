06d5de15912ce3c4f1356961ffbf5f706cff6a31
Simon Crase <simon@greenweaves.nz>
2020-01-27 22:06:10 +1300

    Exercise 3.4: reduced beta - plots look reasonable

--- book/matrix-square.py
+++ book/matrix-square.py
@@ -20,57 +20,45 @@
 import math,matplotlib.pyplot as plt,numpy as np,os
 from matplotlib import rc
 
-m          = 1
-h          = 1
-beta       = 1
-omega      = 1
-step       = 1
-phi_mult   = 1
 phi_helper = {}
 V          = {}
 
 
 @np.vectorize
 def trotter(i,j):
-   rho_free = phi_mult if i==j else phi_mult*phi_helper[abs(i-j)]
-   return V[abs(i)] * rho_free * V[abs(j)]
-   
-def matrix_square(X,Y,beta=1,V=lambda x:0):
-    pass
+   return V[abs(i)] * phi_helper[abs(i-j)] * V[abs(j)]
 
 if __name__=='__main__':
    import argparse
       
    parser = argparse.ArgumentParser('Exercise 3.4: calculate density matrix by matrix squaring')
-   parser.add_argument('--beta',default=0.0001,type=float,help='Inverse temperature')
-   parser.add_argument('--h',default=1,type=float,help='Planck\'s constant')
-   parser.add_argument('-m','--m',default=1.0,type=float,help='Mass of particle')
-   parser.add_argument('--omega',default=1.0,type=float,help='Frequency')
-   parser.add_argument('--n',default=100,type=int,help='Number of steps')
-   parser.add_argument('--L',default=3,type=float,help='Length')
-   parser.add_argument('--show',action='store_true',help='Show plot')
-   parser.add_argument('--rows',default=4,type=int,help='Number of rows to plot')
-   parser.add_argument('--cols',default=4,type=int,help='Number of columns to plot')
+   parser.add_argument('--beta',   default=0.1, type=float,                      help='Inverse temperature')
+   parser.add_argument('--h',      default=1,   type=float,                      help='Planck\'s constant')
+   parser.add_argument('-m','--m', default=1.0, type=float,                      help='Mass of particle')
+   parser.add_argument('--omega',  default=1.0, type=float,                      help='Frequency')
+   parser.add_argument('--n',      default=100, type=int,                        help='Number of steps')
+   parser.add_argument('--L',      default=5,   type=float,                      help='Length')
+   parser.add_argument('--show',                            action='store_true', help='Show plot')
+   parser.add_argument('--rows',   default=4,   type=int,                        help='Number of rows to plot')
+   parser.add_argument('--cols',   default=4,   type=int,                        help='Number of columns to plot')
    
-   args     = parser.parse_args()
-   m        = args.m
-   h        = args.h
-   beta     = args.beta
-   omega    = args.omega   
+   args     = parser.parse_args() 
+
+   beta     = args.beta  
    step     = args.L / args.n
-   phi_mult = math.sqrt(m/(2*math.pi*h*h*beta))
+   phi_mult = math.sqrt(args.m/(2*math.pi*args.h*args.h*beta))
    grid_i   = [i for i in range(-args.n,args.n+1)]
    I,J      = np.meshgrid(grid_i,grid_i)
    grid_x   = [i * step for i in range(-args.n,args.n+1)]
    X,Y      = np.meshgrid(grid_x,grid_x)
    
    for i in range(-args.n,args.n+1):
-      for j in range(i+1,args.n+1):
+      for j in range(i,args.n+1):
          if not (j-i) in phi_helper:
-            phi_helper[(j-i)] = math.exp(- (m * (step*(i-j))**2)/(2*h**2*beta))
+            phi_helper[(j-i)] = phi_mult * math.exp(- (args.m * (step*(i-j))**2)/(2 * args.h**2 * beta))
             
    for i in range(args.n+1):
-      V[i]= math.exp(-0.5 * beta * 0.5 * m * omega **2 * (step * i)**2)
+      V[i]= math.exp(-0.5 * beta * 0.5 * args.m * args.omega  **2 * (step * i)**2)
       
    rho = trotter(I,J)
 
@@ -83,8 +71,7 @@ if __name__=='__main__':
       plt.colorbar()
       plt.title(r'$\rho(x,x^{{\prime}},{0:.4f})$'.format(beta))
       beta *= 2      
-      rho = step * np.matmul(rho, rho)
-
+      rho  = np.dot(step,  np.dot(rho, rho))
    plt.savefig('{0}.png'.format(os.path.splitext(os.path.basename(__file__))[0]))    
    if args.show:
       plt.show()        
