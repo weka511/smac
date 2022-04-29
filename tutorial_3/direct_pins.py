334d4bad0129d3d5e242f63a670d38817f7fc3f2
Simon Crase <simon@greenweaves.nz>
2022-04-29 12:08:09 +1200

    A couple of week 3 programs documented

--- tutorial_3/direct_pins.py
+++ tutorial_3/direct_pins.py
@@ -1,14 +1,17 @@
-import random
+'''Algorithm 6.1: direct sampling'''
 
-N = 15
-L = 10.0
-sigma = 0.2
+from random import uniform
+
+N         = 15
+L         = 10.0
+sigma     = 0.2
 n_configs = 100
+
 for config in range(n_configs):
     n_rejected=0
     x = []
     while len(x) < N:
-        x.append(random.uniform(sigma, L - sigma))
+        x.append(uniform(sigma, L - sigma))
         for k in range(len(x) - 1):
             if abs(x[-1] - x[k]) < 2.0 * sigma:
                 x = []
--- tutorial_3/direct_pins_density.py
+++ tutorial_3/direct_pins_density.py
@@ -1,36 +1,32 @@
-import pylab
+from numpy             import linspace
+from matplotlib.pyplot import plot, xlabel, savefig, show, title, xlabel, ylabel
 
 def binomialCoeff(n, k):
     result = 1
     for i in range(1, k+1):
-        result = result * (n-i+1) / i
+        result *= (n-i+1) / i
     return result
 
 def Z(N, L, sigma):
     freespace = L - 2.0 * N * sigma
     if freespace > 0.0:
-        result = freespace ** N
+        return freespace ** N
     else:
-        result = 0.0
-    return result
+        return 0.0
 
 def pi(x, N, L, sigma):
-    tot = 0.
-    for k in range(0, N):
-        Z1 = Z(k, x - sigma, sigma)
-        Z2 = Z(N - k - 1, L - x - sigma, sigma)
-        tot += binomialCoeff( N - 1, k) * Z1 * Z2
-    Ztotal = Z(N, L, sigma)
-    return tot / Ztotal
+    return sum([binomialCoeff( N - 1, k) * Z(k, x - sigma, sigma) * Z(N - k - 1, L - x - sigma, sigma) for k in range(0, N)]) / Z(N, L, sigma)
 
-L = 20.0
-N = 10
+L     = 20.0
+N     = 10
 sigma = 0.75
-xr = pylab.linspace(0.0, L, 201)
-yr = [pi(x, N, L, sigma) for x in xr]
-pylab.plot(xr, yr, 'red', linewidth=2.0)
-pylab.xlabel('$x$', fontsize=14)
-pylab.ylabel('$\pi(x)$', fontsize=14)
-pylab.title('Exact density of %i clothes-pins ($\sigma$=%s)\non a line of length L=%s' % (N, sigma, L))
-pylab.savefig('plot-direct_pins_density.png')
-pylab.show()
+
+xr     = linspace(0.0, L, 201)
+yr     = [pi(x, N, L, sigma) for x in xr]
+
+plot(xr, yr, 'red', linewidth=2.0)
+xlabel('$x$', fontsize=14)
+ylabel('$\pi(x)$', fontsize=14)
+title('Exact density of %i clothes-pins ($\sigma$=%s)\non a line of length L=%s' % (N, sigma, L))
+savefig('plot-direct_pins_density.png')
+show()
