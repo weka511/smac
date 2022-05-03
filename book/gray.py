3def8a639a0c1cc2fd10e9a069317c8506d697fc
Simon Crase <simon@greenweaves.nz>
2022-05-04 09:33:56 +1200

    #2 Fixed errors in iterator, simplfies, and added generator for Nbrs

--- book/gray.py
+++ book/gray.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Greenweaves Software Limited
+# Copyright (C) 2019-2022 Greenweaves Software Limited
 #
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,81 +13,108 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
-#   grey
-#
-#   Generator for Gray code
-#
-#   Inputs: N
-#
-#   Used to iterate through grey codes, returning
-#   the coordinate that gets flipped each time (1-based) 
-#
-#   Usage:
-#          for k in gray(N):
-#               ....
+'''Algorithm 5.2: Gray code for spins {1,...N}.'''
 
 def gray(N):
+    '''
+    Generator for Gray code
+
+    Inputs: N
+
+    Used to iterate through gray codes, returning
+    the coordinate that gets flipped each time (1-based)
+
+    Usage:
+        for k in gray(N):
+            ....
+    '''
     tau = list(range(1,(N+1)+1))
-    for i in range(2**(N-1)-1):
-        k = tau[0]
+
+    while True:
+        k = tau[0]                   # The next spin to flip (1-based)
         if k>N: return
         tau[k-1] = tau[k]
         tau[k]   = k+1
         if (k != 1): tau[0] = 1
         yield k
 
-# Gray
-#
-# Class for Gray code. Permits restarting
-
 class Gray:
-    def __init__(self,N,tau=None,i=0,progress=0):
-        self.N        = N
-        self.tau      = list(range(1,(N+1)+1)) if tau==None else tau
-        self.i        = i
-        self.max      = 2**(N-1)-1
-        self.progress = progress
-        
+    '''Class for Gray code. Permits restarting'''
+    def __init__(self,N, tau = None):
+        self.N   = N
+        self.tau = list(range(1,(N+1)+1)) if tau==None else tau
+
     def __iter__(self):
         return self
-    
+
     def __next__(self):
-        if self.i<self.max:
-            self.i        += 1
-            k             = self.tau[0]
-            if self.progress>0 and self.i%self.progress==0:
-                print ('{0:.4f}%'.format(100*self.i/self.max))
-            if k>self.N: raise StopIteration
-            self.tau[k-1] = self.tau[k]
-            self.tau[k]   = k+1
-            if (k != 1): self.tau[0] = 1
-            return k            
-        else:
-            raise StopIteration
-   
+        k       = self.tau[0]
+        if k>self.N: raise StopIteration
+        self.tau[k-1] = self.tau[k]
+        self.tau[k]   = k+1
+        if (k != 1): self.tau[0] = 1
+        return k
+
+
     def __str__(self):
         return '['+','.join(str(t) for t in self.tau)+']'
-            
-        
+
+def Nbr(k, shape=(4,5)):
+    '''
+    A generator to iterate through neighbours of a spin
+
+    Parameters:
+        k      index of spin (zero based)
+        shape  Number of rows, columns, etc.
+
+    Test data correspond to
+         15  16  17  18  19
+         10  11  12  13  14
+          5   6   7   8   9
+          0   1   2   3   4
+    '''
+    assert len(shape)==2,'2 D is the only version implemented'
+    m,n = shape
+    i,j = k//n,k%n
+    for i0 in [i-1,i+1]:
+        if i0>-1 and i0<m:
+            yield i0*n + j
+    for j0 in [j-1,j+1]:
+        if j0>-1 and j0 < n:
+            yield i *n + j0
+
+
 if __name__=='__main__':
     import unittest
-    
+
     class GrayTest(unittest.TestCase):
         def setUp(self):
-            flips    = [1,2,1,3,1,2,1]
-            self.expected = flips+[1+max(flips)]+flips[::-1]
-            
+            flips         = [1,2,1,3,1,2,1]
+            self.expected = flips + [1+max(flips)] + flips[::-1]
+
         def testGrayGenerator(self):
-            i        = 0
-            for k in gray(4):
+            '''Test that the returned values are correct, and that there are enough iterations'''
+            for i,k in enumerate(gray(4)):
                 self.assertEqual(self.expected[i],k)
-                i+=1
-                
+            self.assertEqual(2**4-2,i)
+
         def testGrayIterator(self):
-            i        = 0
-            grey     = Gray(4)
-            for k in grey:
+            gray = Gray(4)
+            for i,k in enumerate(gray):
                 self.assertEqual(self.expected[i],k)
-                i+=1
-                
+            self.assertEqual(2**4-2,i)
+
+        def testNbr1(self):
+            Nbrs = list(Nbr(12))
+            self.assertListEqual([7,17,11,13], Nbrs)
+        def testNbr2(self):
+            Nbrs = list(Nbr(14))
+            self.assertListEqual([9,19,13], Nbrs)
+        def testNbr3(self):
+            Nbrs = list(Nbr(15))
+            self.assertListEqual([10,16], Nbrs)
+        def testNbr4(self):
+            Nbrs = list(Nbr(4))
+            self.assertListEqual([9,3], Nbrs)
+
     unittest.main()
