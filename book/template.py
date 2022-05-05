a00f56fe35c0588c7d706921e7db47f3f4f1077f
Simon Crase <simon@greenweaves.nz>
2022-05-06 08:35:20 +1200

    Reformatted and changed style

--- book/template.py
+++ book/template.py
@@ -1,6 +1,4 @@
-# template.py
-
-# Copyright (C) 2020 Greenweaves Software Limited
+# Copyright (C) 2022 Simon Crase
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,32 +13,34 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-# Template for programs
+'''Template for programs--replace with description'''
 
-import math,matplotlib.pyplot as plt,numpy as np,os
-from matplotlib import rc
+from argparse          import ArgumentParser
+from matplotlib.pyplot import figure, plot, savefig,show
+from os.path           import basename, splitext
+from matplotlib        import rc
 
 # Determine plot file name
 
 def get_plot_file_name(plot):
-    if len(plot)==0:
-        return '{0}.png'.format(os.path.splitext(os.path.basename(__file__))[0])
-    base,ext = os.path.splitext(plot)
-    if len(ext)==0:
-        return '{0}.png'.format(plot)
-    return plot
+    if plot==None:
+        return f'{splitext(basename(__file__))[0]}.png'
+    base,ext = splitext(plot)
+    return f'{plot}.png' if len(ext)==0 else plot
 
 if __name__=='__main__':
-    import argparse
-    
-    parser = argparse.ArgumentParser('Template')
-    parser.add_argument('--show',                           action='store_true', help='Show plot')
-    parser.add_argument('--plot', default='',                                   help='Name of plot file')
+    parser = ArgumentParser(description = __doc__)
+    parser.add_argument('--show',
+                        action = 'store_true',
+                        help   = 'Show plot')
+    parser.add_argument('--plot',
+                        default = None,
+                        help    = 'Name of plot file')
     args   = parser.parse_args()
     rc('font',**{'family':'serif','serif':['Palatino']})
     rc('text', usetex=True)
     plt.figure(figsize=(20,20))
     plt.plot([1,2,3])
-    plt.savefig(get_plot_file_name(args.plot))   
+    plt.savefig(get_plot_file_name(args.plot))
     if args.show:
-        plt.show()    
+        plt.show()
