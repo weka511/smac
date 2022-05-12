c5391056e717dfb37060e56964745c952f5d25ea
Simon Crase <simon@greenweaves.nz>
2022-05-13 09:36:16 +1200

    #5 plot Bolzmann curve and velocity vectors

--- book/md-plot.py
+++ book/md-plot.py
@@ -13,13 +13,34 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-'''Template for programs--replace with description'''
+'''Visualize output from md.cpp'''
 
 from argparse          import ArgumentParser
-from matplotlib.pyplot import figure, hist, hist2d, scatter, savefig, show, subplot
+from matplotlib.pyplot import arrow, colorbar, figure, hist, hist2d, legend, plot, savefig, scatter,  show, subplot, tight_layout, title, xlabel, ylabel
+from numpy             import array, exp, log
 from os.path           import basename, splitext
 from matplotlib        import rc
+from scipy.stats       import linregress
 
+def read_input(file_name='md.csv'):
+    Xs=[]
+    Ys=[]
+    Us=[]
+    Vs=[]
+    with open(file_name) as input_file:
+        for line in input_file:
+            x,y,u,v = line.strip().split(",")
+            Xs.append(float(x))
+            Ys.append(float(y))
+            Us.append(float(u))
+            Vs.append(float(v))
+    return Xs,Ys,Us,Vs
+
+def get_curve(n,bins):
+    xs     = array([0.5*(bins[i] + bins[i+1]) for i in range(n.shape[0])])
+    fitted = linregress(xs,log(n))
+    beta   = -fitted.slope
+    return beta,xs,exp(fitted.intercept+fitted.slope*xs)
 
 def get_plot_file_name(plot=None):
     '''Determine plot file name from source file name or command line arguments'''
@@ -36,29 +57,60 @@ def parse_arguments():
     parser.add_argument('--plot',
                         default = None,
                         help    = 'Name of plot file')
+    parser.add_argument('--input',
+                        default = 'md.csv',
+                        help    = 'Name of file produced by md.cpp')
     return parser.parse_args()
 
 if __name__=='__main__':
     args = parse_arguments()
-    Xs=[]
-    Ys=[]
-    Us=[]
-    Vs=[]
-    with open('md.csv') as input_file:
-        for line in input_file:
-            x,y,u,v = line.strip().split(",")
-            Xs.append(float(x))
-            Ys.append(float(y))
-            Us.append(float(u))
-            Vs.append(float(v))
-    Es = [u**2 + v**2 for u,v in zip(Us,Vs)]
+    Xs,Ys,Us,Vs = read_input(file_name=args.input)
+    Es          = [0.5*(u**2 + v**2) for u,v in zip(Us,Vs)]
     rc('font',**{'family':'serif','serif':['Palatino']})
     rc('text', usetex=True)
     figure(figsize=(12,12))
-    ax1 = subplot(1,2,1)
-    ax1.hist2d(Xs,Ys)
-    ax2 = subplot(1,2,2)
-    ax2.hist(Es)
+    subplot(2,2,1)
+    scatter(Xs,Ys,
+            color = 'xkcd:blue')
+    xlabel('X')
+    ylabel('Y')
+    title('Positions')
+
+    subplot(2,2,2)
+    scale = 0.01
+    for x,y,u,v in zip(Xs,Ys,Us,Vs):
+        arrow(x,y,scale*u,scale*v,
+              color      = 'xkcd:blue',
+              head_width = 0.05)
+    xlabel('X')
+    ylabel('Y')
+    title('Velocities')
+
+    subplot(2,2,3)
+    hist2d(Xs,Ys,
+           bins  = 25,
+           range = [[-1,1],[-1,1]])
+    colorbar()
+    xlabel('X')
+    ylabel('Y')
+    title('Density')
+
+    subplot(2,2,4)
+    n,bins,_    = hist(Es,
+                       bins    = 25,
+                       density = True,
+                       color   = 'xkcd:blue',
+                       label   = 'Observed')
+    beta,xs,ys  = get_curve(n,bins)
+    plot(xs,ys,
+         color = 'xkcd:red',
+         label = f'Bolzmann $\\beta T=${beta:.4f}')
+    legend()
+    title('Energies')
+    xlabel('E')
+
+    tight_layout()
+
     savefig(get_plot_file_name(args.plot))
     if args.show:
         show()
