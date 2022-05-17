ffda159eebc166cfd4d61eba408c062405a705f0
Weka <simon@greenweaves.nz>
2022-05-17 23:11:46 +0100

    Close #7. Modified help text

--- book/md/md.cpp
+++ book/md/md.cpp
@@ -226,19 +226,18 @@ bool killed(std::string kill_file){
 
 void help(int N, int n,	int d ,	int M ,	int freq, bool restart,
 			double L, double V,	double sigma,  std::string output_path, std::string restart_path) {
-	cout << "Molecular Dynamics"                                 << endl  << endl;
-	cout << "    Parameters"                                     << endl;
-	cout << "\tN\tNumber of iterations\t\t\t\t"                  << N     << endl;
-	cout << "\tn\tNumber of spheres\t\t\t\t"                     << n     << endl;
-	cout << "\td\tDimension of box\t\t\t\t"                      << d     << endl;
-	cout << "\tM\tNumber of attempts to build configuration\t"   << M     << endl;
-	cout << "\tfreq\tFrequency for indicating progress\t\t"      << freq  << endl;
-	cout << "\trestart\tSet if Configuration is to be restarted" << endl;
-	cout << "\tL\tLength of side of box\t\t\t\t"                 << L     << endl;
-	cout << "\tV\tNormalizer of initial velocity\t\t\t"          << V     << endl;
-	cout << "\tsigma\tRadius of sphere\t\t\t\t"                  << sigma << endl;
-	cout << "\toutput_path" <<endl;
-	cout << "\trestart_path" <<endl;
+	cout << "Molecular Dynamics"                                 << endl        << endl;
+	cout << "    Parameters"                                                    << endl;
+	cout << "\tN\tNumber of iterations\t\t\t\t"                  << N           << endl;
+	cout << "\tn\tNumber of spheres\t\t\t\t"                     << n           << endl;
+	cout << "\td\tDimension of box\t\t\t\t"                      << d           << endl;
+	cout << "\tM\tNumber of attempts to build configuration\t"   << M           << endl;
+	cout << "\tfreq\tFrequency for indicating progress\t\t"      << freq        << endl;
+	cout << "\tL\tLength of side of box\t\t\t\t"                 << L           << endl;
+	cout << "\tV\tNormalizer of initial velocity\t\t\t"          << V           << endl;
+	cout << "\tsigma\tRadius of sphere\t\t\t\t"                  << sigma       << endl;
+	cout << "\toutput\tPath name for output file\t\t\t"          << output_path << endl;
+	cout << "\trestart\tRestart run from file\t\t\t"                            << endl;
 }
 
 
