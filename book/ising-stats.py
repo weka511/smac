1f0dbf0b1e1f35ffa3af3a1aa6a6286b0c9ece96
Simon Crase <simon@greenweaves.nz>
2025-03-13 10:13:26 +1300

    #25 Started port to numpy

--- book/ising-stats.py
+++ book/ising-stats.py
@@ -1,4 +1,6 @@
-# Copyright (C) 2019-2022 Greenweaves Software Limited
+#!/usr/bin/env python
+
+# Copyright (C) 2019-2025 Greenweaves Software Limited
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,82 +15,86 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
-''' 	Figure 6.6 - plot data from ising.py'''
+'''Figure 6.6 - plot data from ising.py'''
 
-from argparse          import ArgumentParser
-from math              import exp,sqrt,log
+from argparse import ArgumentParser
 from matplotlib.pyplot import axvline, figure, legend, plot, show, title, xlabel, ylabel
-from numpy             import arange
+import numpy as np
 
-
-def thermo(N,Es,Ns,beta=1.0):
-    Emin   = min(Es)
-    Eprime = [E-Emin for E in Es]
-    Z      = sum([N         * exp(-beta*E) for (N,E) in zip(Ns,Eprime)])
-    Emean  = sum([N * E     * exp(-beta*E) for (N,E) in zip(Ns,Eprime)])/Z
-    Esq    = sum([N * E * E * exp(-beta*E) for (N,E) in zip(Ns,Eprime)])/Z
-    Z      = Z*exp(-beta*Emin)
-    return (Z, beta*beta*(Esq-Emean*Emean)/N, (Emean + Emin)/N)
-
+def thermo(N,E,beta=1.0):
+    '''Algorithm 5.4 Calculate thermodynamic quantities'''
+    Emin = E.min()
+    Eprime = E - Emin
+    weights = np.exp(-beta*Eprime)
+    Z = np.dot(N, weights)
+    Emean = np.dot(N,Eprime*weights)/Z
+    Esq = np.dot(N,Eprime*Eprime*weights)/Z
+    Z = Z*np.exp(-beta*Emin)
+    return (Z, (Emean + Emin)/len(N), beta**2*(Esq - Emean**2)/len(N) ) # Z, <e>, cV
 
 parser = ArgumentParser(description='Compute statistics for Ising model')
 parser.add_argument('-i', '--input',
                     default = 'ising.csv',
-                    help    = 'File to record results')
+                    help  = 'File to record results')
 args = parser.parse_args()
 
-with open(args.input) as f:
-    # def agg(m0):
-        # return sum([n for (m,n) in zip(Ms,Ns) if m0==m])
-    # i  = 0
-    Es = []
-    Ms = []
-    Ns = []
-    N  = None
-    pi = {}
-    for i,line in enumerate(f):
-        if i==0:
-            continue
-        else:
-            values=[int(s) for s in line.strip().split(',')]
-            Es.append(values[0])
-            Ms.append(values[1])
-            Ns.append(values[2])
-            if not values[0] in pi:
-                pi[values[0]]=[]
-            pi[values[0]].append((values[1],values[2]))
-        i+=1
-    Ts  = []
-    cvs = []
-    for t in arange(0.8, 6.0, 0.05):
-        _,cv,E = thermo(N,Es,Ns,beta=1/t)
-        Ts.append(t)
-        cvs.append(cv)
+ff = np.genfromtxt(args.input, delimiter=',')
+E = ff[1:,0]
+N = ff[1:,1]
 
-    plot(Ts,cvs,color='b')
-    axvline(x=2/log(1+sqrt(2)),color='r',linestyle='--')
-    xlabel('Temperature')
-    ylabel('Specific Heat Capacity')
-    title('Thermodynamic quantities')
+# with open(args.input) as f:
+    # Es = []
+    # Ms = []
+    # Ns = []
+    # N  = 1# FIXME None
+    # pi = {}
+    # for i,line in enumerate(f):
+        # if i == 0:
+            # continue
+        # else:
+            # parts = line.strip().split(',')
+            # Es.append(int(parts[0]))
+            # Ms.append(int(parts[1]))
+            # Ns.append(float(parts[0]))
+            # if not Es[-1] in pi:
+                # pi[Es[-1]] = []
+            # pi[Es[-1]].append((Ms[-1],Ns[-1]))
+        # i += 1
+T = np.linspace(0.8,6.0)
+cV = np.array([thermo(N,E,beta=1/t)[2] for t in T])
+# for t in np.arange(0.8, 6.0, 0.05):
+    # _,_,cv = thermo(N,E,beta=1/t)
+    # Ts.append(t)
+    # cvs.append(cv)
 
-    figure(figsize=(10,10))
-    colours     = ['r','g','b','m','y','c','k']
-    line_styles = ['--',':','-.']
-    index       = 0
+fig = figure(figsize=(10,10))
+ax1 = fig.add_subplot(2,1,1)
+ax1.plot(T,cV,color='b',label='$c_V$')
+ax1.axvline(x=2/np.log(1+np.sqrt(2)),color='r',linestyle='--')
+ax1.set_xlabel('Temperature')
+ax1.set_ylabel('Specific Heat Capacity')
+ax1.set_title('Thermodynamic quantities')
+ax1.legend()
 
-    for E in sorted(pi.keys()):
-        stats         = sorted(pi[E])
-        magnetization = [m for m,_ in stats]
-        counts        = [n for _,n in stats]
-        total         = sum(counts)
-        frequency     = [n/total for n in counts]
-        plot(magnetization,frequency,
-                 color=colours[index%len(colours)],
-                 label='{0}'.format(E),ls=line_styles[index//len(colours)])
-        index+=1
+# figure(figsize=(10,10))
+# colours     = ['r','g','b','m','y','c','k']
+# line_styles = ['--',':','-.']
+# index       = 0
 
-    xlabel('Magnetization')
-    ylabel('Frequency')
-    title('Frequency of Magnetization as a function of Energy')
-    legend(title='Energy')
-    show()
+    # for E in sorted(pi.keys()):
+        # stats         = sorted(pi[E])
+        # magnetization = [m for m,_ in stats]
+        # counts        = [n for _,n in stats]
+        # total         = sum(counts)
+        # if total == 0: total += 1
+        # frequency     = [n/total for n in counts]
+        # plot(magnetization,frequency,
+                 # color=colours[index%len(colours)],
+                 # label='{0}'.format(E),ls=line_styles[index//len(colours)])
+        # index+=1
+
+# xlabel('Magnetization')
+# ylabel('Frequency')
+# title('Frequency of Magnetization as a function of Energy')
+# legend(title='Energy')
+show()
