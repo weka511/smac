6283962b2e0aee8d1a7db61394aaa6f9acdd8779
Simon Crase <simon@greenweaves.nz>
2025-03-15 11:18:32 +1300

    Started #28

---
+++ book/AllTests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from unittest import main
+
+from gray import NbrTest, GrayGeneratorTest, GrayFlipTest
+from enumerate_ising import TestIsing
+from geometry import TestHistogram
+
+main()
