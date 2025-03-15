5b767fc040758300e2237561fd0335ec521e23a6
Simon Crase <simon@greenweaves.nz>
2025-03-16 12:42:37 +1300

    #30 Align template.py with other projects

--- book/template.py
+++ book/template.py
@@ -1,51 +1,55 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2025 Simon Crase
+#   Copyright (C) 2024 Simon Crase
 
-# This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-'''Template for programs--replace with description'''
+''' Template for Python programs'''
 
 from argparse import ArgumentParser
+from os.path import basename, join, splitext
+from time import time
+import numpy as np
 from matplotlib import rc
-from matplotlib.pyplot import figure, plot, savefig, show
-from os.path import basename, splitext
+from matplotlib.pyplot import figure, show
 
-def get_plot_file_name(plot=None):
-    '''Determine plot file name from source file name or command line arguments'''
-    if plot==None:
-        return f'{splitext(basename(__file__))[0]}.png'
-    base,ext = splitext(plot)
-    return f'{plot}.png' if len(ext)==0 else plot
 
 def parse_arguments():
-    parser = ArgumentParser(description = __doc__)
-    parser.add_argument('--show',
-                        action = 'store_true',
-                        help   = 'Show plot')
-    parser.add_argument('--plot',
-                        default = None,
-                        help    = 'Name of plot file')
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('--figs', default = './figs')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
     return parser.parse_args()
 
-if __name__=='__main__':
-    args = parse_arguments()
+def get_file_name(figs):
+    return join(figs,basename(splitext(__file__)[0]))
 
+if __name__=='__main__':
     rc('font',**{'family':'serif','serif':['Palatino']})
     rc('text', usetex=True)
-    figure(figsize=(12,12))
-    plot([1,2,3])
-    savefig(get_plot_file_name(args.plot))
+    start  = time()
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    args = parse_arguments()
+    rng = np.random.default_rng(args.seed)
+    fig = figure(figsize=(12,12))
+
+    fig.savefig(get_file_name(args.figs))
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
     if args.show:
         show()
