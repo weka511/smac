99f341d4c94a2631ff6d3ac4142289caa02ca214
Simon Crase <simon@greenweaves.nz>
2025-03-16 08:17:02 +1300

    #29

--- exam/Q1.py
+++ exam/Q1.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2018-2025 Greenweaves Software Limited
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
 import random
 
 L = 3
@@ -21,7 +38,7 @@ for i in range(1024):
             transitions[(s0,s1,site[0],site[1])]+=1
         else:
             transitions[(s0,s1,site[0],site[1])]=0
-             
+
     #print (site)
 
 #for i in range(L):
--- exam/Q7.py
+++ exam/Q7.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2018-2025 Greenweaves Software Limited
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
 import random,matplotlib.pyplot as plt
 
 L = 4
@@ -13,5 +30,5 @@ for step in range(nsteps):
     if x + dx >= 0 and x + dx < L:
         x += dx
     xs.append(x)
-   
+
 plt.hist(xs)
