66660a874f1fa242d1e8a81dbb55b9965fede87b
Simon Crase <simon@greenweaves.nz>
2025-03-27 10:26:12 +1300

    #36 Verified that thermo_ising replicates Table 5.3

--- lecture_8/thermo_ising.py
+++ lecture_8/thermo_ising.py
@@ -19,17 +19,20 @@ import math, os
 
 L = 6
 N = L * L
-filename = 'data_dos_L%i.txt' % L
+filename = '../book/6x6.csv'
 if os.path.isfile(filename):
     dos = {}
     f = open(filename, 'r')
     for line in f:
-        E, N_E = line.split()
-        dos[int(E)] = int(N_E)
+        try:
+            E, N_E = line.strip().split(',')
+            dos[int(E)] = int(N_E)
+        except ValueError as v:
+            print (v)
     f.close()
 else:
     exit('input file missing')
-list_T = [0.5 + 0.5 * i for i in range(10)]
+list_T = [0.5 + 0.5 * i for i in range(8)]
 for T in list_T:
     Z = 0.0
     E_av = 0.0
@@ -43,4 +46,4 @@ for T in list_T:
     E2_av /= Z
     E_av /= Z
     cv = (E2_av - E_av ** 2) / N / T ** 2
-    print (T, E_av / float(N), cv)
+    print (f'{T}, {E_av / float(N):.3f}, {cv:.5f}')
