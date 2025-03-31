eca2d2b7d522ff58cd53422eca4edbdc87fd2a96
Simon Crase <simon@greenweaves.nz>
2025-03-31 18:02:49 +1300

    #44 Created skeleton

---
+++ book/exercise_5_2.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2024-2025 Simon Crase
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+''' Template for Python programs'''
+
+from argparse import ArgumentParser
+from os.path import basename, join, splitext
+from time import time
+import numpy as np
+from matplotlib import rc
+from matplotlib.pyplot import figure, show
+
+
+def parse_arguments():
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--periodic', default=False, action = 'store_true', help = 'Use periodic boundary conditions')
+    parser.add_argument('-m', type = int, default = 4, help = 'Number of rows')
+    parser.add_argument('-n', type = int, default = 4, help = 'Number of columns')
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
+    parser.add_argument('--figs', default = './figs')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    return parser.parse_args()
+
+
+def get_file_name(args,default_ext='.png',seq=None):
+    '''
+    Used to create file names
+
+    Parameters:
+        args
+        default_ext
+        seq
+    '''
+    base,ext = splitext(args.out)
+    if len(ext) == 0:
+        ext = default_ext
+    if seq != None:
+        base = f'{base}{seq}'
+    return join(args.figs,f'{base}{ext}')
+
+def create_spins(N):
+    Spins = np.zeros((2**N,N))
+    for i in range(2**N):
+        j = i
+        k = 0
+        while j > 0:
+            k -= 1
+            j,Spins[i,k] = divmod(j,2)
+    return Spins
+
+if __name__=='__main__':
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    start  = time()
+    args = parse_arguments()
+
+    N = args.m*args.n
+    print (create_spins(N))
+    rng = np.random.default_rng(args.seed)
+    fig = figure(figsize=(12,12))
+
+    fig.savefig(get_file_name(args))
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
