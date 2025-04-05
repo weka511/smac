e1c6621e75d881b429284a50ee33a0eb8459882d
Simon Crase <simon@greenweaves.nz>
2025-04-05 16:21:32 +1300

    #47 Started

---
+++ book/exercise_5_4.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2024-2025 Simon Crase
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+''' Template for Python programs'''
+
+from argparse import ArgumentParser
+from os.path import basename, join, splitext
+from time import time
+import numpy as np
+from matplotlib import rc
+from matplotlib.pyplot import figure, show
+
+
+def parse_arguments():
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('-i', '--input', default = r'C:\cygwin64\home\Weka\smac\book\ising\out.txt',help='Name of input file')
+    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
+    parser.add_argument('--figs', default = './figs')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    return parser.parse_args()
+
+
+def get_file_name(name,default_ext='png',seq=None):
+    '''
+    Used to create file names
+
+    Parameters:
+        name          Basis for file name
+        default_ext   Extension if non specified
+        seq           Used if there are multiple files
+    '''
+    base,ext = splitext(name)
+    if len(ext) == 0:
+        ext = default_ext
+    if seq != None:
+        base = f'{base}{seq}'
+    qualified_name = f'{base}.{ext}'
+    if ext == 'png':
+        return join(args.figs,qualified_name)
+    else:
+        return qualified_name
+
+def get_data(file_name):
+    data = []
+    with open(file_name) as input:
+        i = 0
+        data = []
+        for line in input:
+            match (i):
+                case 0:
+                    pass
+                case 1:
+                    pass
+                case _:
+                    data.append([int(j) for j in line.split(',')])
+            i += 1
+        return np.array(data,dtype=int)
+
+def create_E(data):
+    def get_upper_limit(i):
+        return indices[i+1] if i+1 < len(indices) else m
+
+    m,_ = data.shape
+    Es,indices = np.unique(data[:,0],return_index=True)
+    product = np.zeros((len(Es),2),dtype=int)
+    for i in range(len(Es)):
+        product[i,0] = Es[i]
+        product[i,1] = sum(data[j,2] for j in range(indices[i],get_upper_limit(i)))
+    return product
+
+if __name__=='__main__':
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    start  = time()
+    args = parse_arguments()
+    rng = np.random.default_rng(args.seed)
+    data = get_data(args.input)
+
+    E = create_E(data)
+    print (E)
+    fig = figure(figsize=(12,12))
+
+    fig.savefig(get_file_name(args.out))
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
