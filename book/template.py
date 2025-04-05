5cb39364d869d851f34615e640ee07ac88b9bcfa
Simon Crase <simon@greenweaves.nz>
2025-04-05 18:42:20 +1300

    #47 Fixed copyright notice

--- book/template.py
+++ book/template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#   Copyright (C) 2024-2025 Simon Crase
+#   Copyright (C) 2025 Simon Crase
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
