0a03b2efad9559d6a72a356c6d8b9d7875f2ad55
Simon Crase <simon@greenweaves.nz>
2025-04-06 15:28:32 +1200

    #38 Plot magnetization

--- book/exercise_5_10b.py
+++ book/exercise_5_10b.py
@@ -23,16 +23,40 @@ from time import time
 import numpy as np
 from matplotlib import rc
 from matplotlib.pyplot import figure, show
-
+from markov_ising import MarkovIsing
 
 def parse_arguments():
     parser = ArgumentParser(__doc__)
+    parser.add_argument('--periodic', default=False, action = 'store_true', help = 'Use periodic boundary conditions')
+    parser.add_argument('-m', type = int, default = 4, help = 'Number of rows')
+    parser.add_argument('-n', type = int, default = 4, help = 'Number of columns')
+    parser.add_argument('--Nsteps', type = int, default = 10000, help = 'Number of steps')
+    parser.add_argument('--Nburn', type = int, default = 0, help = 'Number of steps for burn in')
+    parser.add_argument('--Niterations', type = int, default = 5, help = 'Number of iterations of Markov chain')
+    parser.add_argument('-f', '--frequency',type = int, default = 100, help = 'How often to report progress')
+    parser.add_argument('-T', '--T', default=[1000], nargs='+', type=float, help = 'Range for temperature: [start, ]stop, [step, ]')
     parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
     parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
     parser.add_argument('--figs', default = './figs')
-    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    parser.add_argument('--show', default=False, action = 'store_true', help = 'Show plot')
     return parser.parse_args()
 
+def get_range(T,deltaT=0.1):
+    '''
+    Used to convert temperature (specified in args) to a range
+
+    Parameters:
+        T       [start, ]stop, [step, ]
+    '''
+    match (len(T)):
+        case 1:
+            return T
+        case 2:
+            return np.arange(T[0],T[1],deltaT)
+        case 3:
+            return np.arange(T[0],T[1]+T[2],T[2])
+
+    raise ValueError(f'Parameter T must have length of 1,2, or 3')
 
 def get_file_name(name,default_ext='png',seq=None):
     '''
@@ -54,15 +78,44 @@ def get_file_name(name,default_ext='png',seq=None):
     else:
         return qualified_name
 
+def get_boundary_conditions(periodic):
+    '''
+    Used to format suptitle
+    '''
+    return 'Periodic boundary conditions' if periodic else 'Bounded'
+
 if __name__=='__main__':
     rc('font',**{'family':'serif','serif':['Palatino']})
     rc('text', usetex=True)
     start  = time()
     args = parse_arguments()
-    rng = np.random.default_rng(args.seed)
-    fig = figure(figsize=(12,12))
 
-    fig.savefig(get_file_name(args.out))
+    rng = np.random.default_rng(args.seed)
+    T_range = get_range(args.T)
+    fig = figure(figsize=(12,12))
+    fig.suptitle(fr'{get_boundary_conditions(args.periodic)} , ' +
+                 fr'{args.m}$\times${args.n} sites, {args.Niterations}$\times${args.Nsteps} steps')
+
+    ax2 = fig.add_subplot(1,1,1)
+    for j,T in enumerate(T_range):
+        markov = MarkovIsing(rng=rng,
+                             shape=(args.m,args.n),
+                             periodic=args.periodic,
+                             Niterations=args.Niterations,
+                             beta=1/T)
+        for i in range(args.Niterations):
+            markov.run(Nsteps=args.Nsteps,Nburn=args.Nburn,frequency=args.frequency,iteration=i)
+
+        Es,means, stds,M,magnetization = markov.get_stats()
+        ax2.bar(M,magnetization,width=0.8,label=f'T={T}')
+        ax2.set_xlabel('$M$')
+        ax2.set_ylabel('Magnetization')
+        ax2.set_title('Magnetization')
+        ax2.legend(loc='upper left')
+
+        fig.tight_layout(h_pad=4,pad=2)
+        fig.savefig(get_file_name(args.out))
+
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
@@ -70,3 +123,4 @@ if __name__=='__main__':
 
     if args.show:
         show()
+
