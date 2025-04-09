f9472c248183e948f5799315d84fdd1de3494292
Weka <simon@greenweaves.nz>
2025-04-10 08:08:23 +1200

    #50 tweaked include file

--- book/ising/enumerate-ising.hpp
+++ book/ising/enumerate-ising.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2019 Greenweaves Software Limited
+ * Copyright (C) 2019-2025 Greenweaves Software Limited
  *
  * This is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,5 +35,4 @@ int get_field(	int sigma[],
 				int n,
 				bool wrapped = false);
 
-#endif
-
+#endif //_ENUMERATE_ISING_HPP_
