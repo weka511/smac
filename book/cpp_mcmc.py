39949274cec19fb953d38e559a33aa1a22b5c0d8
Simon Crase <simon@greenweaves.nz>
2025-04-10 16:58:20 +1200

    #54 Added decorations to plots

--- book/cpp_mcmc.py
+++ book/cpp_mcmc.py
@@ -27,7 +27,6 @@ from re import compile
 
 def parse_arguments():
     parser = ArgumentParser(__doc__)
-    parser.add_argument('--seed',type=int,default=None,help='Seetd for random number generator')
     parser.add_argument('-i', '--input', default = 'markov-out.txt',help='Name of input file')
     parser.add_argument('-p', '--path', default = r'C:\cygwin64\home\Weka\smac\book\ising',help='path to file')
     parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
@@ -85,26 +84,35 @@ def read_file(path,input):
                     parts = line.split(',')
                     try:
                         Ms.append((int(parts[0]),int(parts[1])))
-                    except ValueError:#beta=0.3333, acceptance=43.326%
+                    except ValueError:
                         match = pattern2.match(line)
-                        accept = float(match.group(1))
+                        accept = float(match.group(2))
+
     return m,n,periodic,beta,np.array(Es),np.array(Ms),accept
 
-
+def get_periodic(is_periodic):
+    return 'periodic' if is_periodic == 'periodic' else 'aperiodic'
 
 if __name__=='__main__':
     rc('font',**{'family':'serif','serif':['Palatino']})
     rc('text', usetex=True)
     start  = time()
     args = parse_arguments()
-    rng = np.random.default_rng(args.seed)
-    m,n,periodic,beta,E,M,accept = read_file(args.path,args.input)
+
+    m,n,is_periodic,beta,E,M,accept = read_file(args.path,args.input)
 
     fig = figure(figsize=(12,12))
+    fig.suptitle(fr'{m}$\times${n},{get_periodic(is_periodic)},$\beta=${beta},acceptance={accept}')
     ax1 = fig.add_subplot(2,1,1)
     ax1.bar(E[:,0],E[:,1]/E[:,1].sum())
+    ax1.set_xlabel('E')
+    ax1.set_ylabel('Frequency')
+
     ax2 = fig.add_subplot(2,1,2)
     ax2.bar(M[:,0],M[:,1]/M[:,1].sum())
+    ax2.set_xlabel('M')
+    ax2.set_ylabel('Frequency')
+
     fig.savefig(get_file_name(args.out))
     elapsed = time() - start
     minutes = int(elapsed/60)
