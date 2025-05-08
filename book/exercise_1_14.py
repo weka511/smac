9096dff835a5f1f9d4002a51a1f15feeefccfd8b
Simon Crase <simon@greenweaves.nz>
2025-05-09 11:59:45 +1200

    #67 Allow Sum of arbitrary number of squares

--- book/exercise_1_14.py
+++ book/exercise_1_14.py
@@ -16,8 +16,10 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 '''
-    Exercise 1.14 Sample random vectors on the surface of a sphere using Algorithm 1.22
-    and plot x[0]**2 + y[0]**2
+    Exercise 1.14 Sample random vectors on the surface of a hypersphere using Algorithm 1.22
+    and plot x[0]**2 + y[0]**2.
+    I have allowed a polynomial of arbitrary order to be plotted. The plots are prepared
+    for a range of values of the dimension, organized into rows and columns.S
 '''
 
 from argparse import ArgumentParser
@@ -28,9 +30,16 @@ from matplotlib import rc
 from matplotlib.pyplot import figure, show
 
 def direct_surface(d,rng = np.random.default_rng(None)):
+    '''
+    Sample random vectors on the surface of a sphere using Algorithm 1.22
+
+    Parameters:
+        d        Dimension of sphere
+        rng      Random number generator
+    '''
     sigma = 1/np.sqrt(d)
     while True:
-        x = rng.normal(size=(d))
+        x = rng.normal(scale=sigma,size=(d))
         Sigma = np.square(x).sum()
         yield x/np.sqrt(Sigma)
 
@@ -38,12 +47,13 @@ def parse_arguments():
     parser = ArgumentParser(__doc__)
     parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
     parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
-    parser.add_argument('--figs', default = './figs')
+    parser.add_argument('--figs', default = './figs',help='Folder for storing plot')
     parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
-    parser.add_argument('--rows',type=int,default=3)
-    parser.add_argument('--columns',type=int,default=3)
-    parser.add_argument('-n','--n',type=int,default=1000)
-    parser.add_argument('-m','--m',type=int,default=100)
+    parser.add_argument('--rows',type=int,default=3,help = 'Number of rows to be plotted')
+    parser.add_argument('--columns',type=int,default=3,help = 'Number of columns to be plotted')
+    parser.add_argument('-n','--n',type=int,default=1000, help='Number of points to be sampled')
+    parser.add_argument('-m','--m',type=int,default=100,help='Number of bins for histogram')
+    parser.add_argument('-k','--k',type=int,default=2,help='Degree of polynomial to be sampled')
     return parser.parse_args()
 
 
@@ -75,15 +85,17 @@ if __name__=='__main__':
     rng = np.random.default_rng(args.seed)
     fig = figure(figsize=(12,12))
     for i in range(args.rows*args.columns):
-        I12 = np.zeros((args.n))
-        ds = direct_surface(d=i+3,rng=rng)
+        d = i + 1 + args.k
+        SumSquares = np.zeros((args.n))
+        ds = direct_surface(d=d,rng=rng)
         for j in range(args.n):
             x = next(ds)
-            I12[j] = x[0]**2 + x[1]**2
+            SumSquares[j] = np.square(x[0:args.k]).sum()
         ax = fig.add_subplot(args.rows,args.columns,i+1)
-        ax.hist(I12,bins=args.m,color='blue',density=True)
-        ax.set_title(f'd={i+3}')
+        ax.hist(SumSquares,bins=args.m,color='blue',density=True)
+        ax.set_title(f'd={d}, k={args.k}')
 
+    fig.tight_layout(h_pad=2)
     fig.savefig(get_file_name(args.out))
     elapsed = time() - start
     minutes = int(elapsed/60)
