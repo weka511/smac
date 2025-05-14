48451684d5b5accaf7792b0ae88031d39decd9fd
Simon Crase <simon@greenweaves.nz>
2025-05-14 15:12:55 +1200

    Started #70

--- book/binomialconvolution.py
+++ book/binomialconvolution.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2015-2022 Greenweaves Software Limited
+# Copyright (C) 2015-2025 Greenweaves Software Limited
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,10 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
-from math              import pi
-from matplotlib.pyplot import plot, savefig, show
-from os                import name
-from os.path           import basename, splitext
+'''Algorithm 1.25 from Krauth'''
+
+from os.path import basename, splitext
+import numpy as np
+from matplotlib.pyplot import figure, show
 
 def get_plot_file_name(plot=None):
     '''Determine plot file name from source file name or command line arguments'''
@@ -27,20 +28,29 @@ def get_plot_file_name(plot=None):
     base,ext = splitext(plot)
     return f'{plot}.png' if len(ext)==0 else plot
 
-def binomial_convolution(theta,estimates):
+def binomial_convolution(theta = np.pi/4,N=9):
     '''Algorithm 1.25 from Krauth'''
-    def calculate_row(estimates):
-        return [theta*x + (1-theta)*y for (x,y) in zip(estimates[:-1],estimates[1:])]
-    return calculate_row([0] + estimates + [0])
+    P = np.full((N,N),np.nan)   # P[N,k] - k hits in N trials
+    P[0,0] = 1
+    for n in range(1,N):
+        P[n,0] = (1-theta) * P[n-1,0]
+        for k in range(1,n):
+            P[n,k] = (1-theta) * P[n-1,k] +  theta* P[n-1,k-1]
+        P[n,n] = theta * P[n-1,n-1]
+
+    return P
 
 
-if __name__=="__main__":
-    pi_estimates = [1]
-    for i in range(8):
-        pi_estimates = binomial_convolution(pi/4,pi_estimates)
-        plot(pi_estimates)
+if __name__=='__main__':
+    theta = np.pi/4              # p hit
+    fig = figure()
+    ax = fig.add_subplot(1,1,1)
 
-    if name == 'posix':
-        savefig(get_plot_file_name())
-    if name == 'nt':
-        show()
+    P = binomial_convolution(theta = np.pi/4,N=1000)
+    ax.plot(P[-1,:])
+    # for n in range(1,9):
+        # ax.plot(P[n,:],label=f'n={n}')
+    # ax.legend()
+
+    fig.savefig(get_plot_file_name())
+    show()
