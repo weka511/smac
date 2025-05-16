285c0dda3092c93e750e0be5709eb7f74bbbe759
Simon Crase <simon@greenweaves.nz>
2025-05-16 13:37:47 +1200

    #71 Revised code style

--- book/direct-disks.py
+++ book/direct-disks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2022 Simon Crase
+# Copyright (C) 2022-2025 Simon Crase
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,116 +22,119 @@
     periodic boundary conditions
 '''
 
-from argparse          import ArgumentParser
-from geometry          import GeometryFactory
-from matplotlib        import rc
-from matplotlib.pyplot import figure, legend, plot, savefig, show, title, xlabel, ylabel, ylim
-from numpy             import array, histogram, reshape
-from os.path           import basename, splitext
-from numpy.random      import default_rng
+from argparse import ArgumentParser
+from os.path import basename, join, splitext
+from sys import maxsize
+from time import time
+import numpy as np
+from matplotlib import rc
+from matplotlib.pyplot import figure, show
+from geometry import GeometryFactory
 
-def direct_disks(sigma    = 0.25,
-                 N        = 4,
-                 NTrials  = float('inf'),
-                 d        = 2,
-                 geometry = None,
-                 rng      = default_rng()):
-    '''Prepare one admissable configuration of disks'''
+def direct_disks(sigma = 0.25, N = 4, NTrials = maxsize, d = 2, geometry = None, rng = np.random.default_rng()):
+    '''
+    Prepare one admissable configuration of disks
+
+    Parameters:
+        sigma
+        N
+        NTrials
+        d
+        geometry
+        rng
+
+    Returns:
+        X, an array containing the coordinates of N points such that we can position N
+        disks of radius sigma, with the centre of each disk at the corresponding point
+        of X.
+    '''
 
     def is_overlapped(X):
         '''Determine whether spheres overlap'''
         for i in range(N):
             for j in range(i+1,N):
-                if geometry.get_distance(X[i,:],X[j,:])<2*sigma:
+                if geometry.get_distance(X[i,:],X[j,:]) < 2*sigma:
                     return True
         return False
 
-    k = 0
-    while True:
+    for k in range(NTrials):
         X = geometry.LowerBound + geometry.UpperBound * rng.random(size=(N,d))
-        if is_overlapped(X):
-            if k>NTrials:
-                raise Exception(f'Failed to place {N} spheres within {NTrials} attempts for sigma={sigma}')
-            else:
-                k += 1
-        else:
-            return X
+        if not is_overlapped(X): return X
 
+    raise RuntimeError(f'Failed to place {N} spheres within {NTrials} attempts for sigma={sigma}')
 
+def get_file_name(name,default_ext='png',seq=None):
+    '''
+    Used to create file names
 
-def get_plot_file_name(plot=None):
-    '''Determine plot file name from source file name or command line arguments'''
-    if plot==None:
-        return f'{splitext(basename(__file__))[0]}.png'
-    base,ext = splitext(plot)
-    return f'{plot}.png' if len(ext)==0 else plot
+    Parameters:
+        name          Basis for file name
+        default_ext   Extension if non specified
+        seq           Used if there are multiple files
+    '''
+    base,ext = splitext(name)
+    if len(ext) == 0:
+        ext = default_ext
+    if seq != None:
+        base = f'{base}{seq}'
+    qualified_name = f'{base}.{ext}'
+    if ext == 'png':
+        return join(args.figs,qualified_name)
+    else:
+        return qualified_name
 
 def parse_arguments():
     parser = ArgumentParser(description = __doc__)
-    parser.add_argument('--N',
-                        type    = int,
-                        default = 10000)
-    parser.add_argument('--Disks',
-                        type    = int,
-                        default = 4)
-    parser.add_argument('--sigma',
-                        type    = float,
-                        nargs   = '+',
-                        default = [0.25])
-    parser.add_argument('--d',
-                        type    = int,
-                        default =2)
-    parser.add_argument('--show',
-                        action = 'store_true',
-                        help   = 'Show plot')
-    parser.add_argument('--plot',
-                        default = None,
-                        help    = 'Name of plot file')
-    parser.add_argument('--bins',
-                        type    = int,
-                        default = 100,
-                        help    = 'Number of bins for histogram')
-    parser.add_argument('--periodic',
-                        action = 'store_true',
-                        default = False)
-    parser.add_argument('--L',
-                        type    = float,
-                        nargs   = '+',
-                        default = [1])
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
+    parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
+    parser.add_argument('--N', type = int, default = 10000, help='Number of configurations to be tried')
+    parser.add_argument('--NTrials', type = int, default = maxsize, help='Number of attempts to create configuration')
+    parser.add_argument('--Disks', type = int, default = 4, help='Number of disks in each configuration')
+    parser.add_argument('--sigma', type = float,  nargs   = '+',  default = [0.25],  help='Radius of a disk')
+    parser.add_argument('--d', type = int, default =2,  help='Dimensionality of space')
+    parser.add_argument('--show', action = 'store_true', help = 'Show plot')
+    parser.add_argument('--bins', type = int, default = 100, help = 'Number of bins for histogram')
+    parser.add_argument('--periodic', action = 'store_true', default = False, help='Use periodic boundary conditions')
+    parser.add_argument('--L', type = float, nargs   = '+', default = [1], help='Lenghts of walls')
     return parser.parse_args()
 
 if __name__=='__main__':
-    args           = parse_arguments()
-    upper_limit    = -float('inf')
-    rng            = default_rng()
-    figure(figsize = (12,12))
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    start  = time()
+    args = parse_arguments()
+    upper_limit = -float('inf')
+    rng = np.random.default_rng(args.seed)
+    sides  = np.array(args.L if len(args.L)==args.d else args.L * args.d)
+    fig = figure(figsize = (12,12))
+    ax1 = fig.add_subplot(1,1,1)
     for sigma in args.sigma:
-        geometry = GeometryFactory(periodic = args.periodic,
-                                   L        = array(args.L if len(args.L)==args.d else args.L * args.d),
-                                   sigma    = sigma,
-                                   d        = args.d)
-        eta      = geometry.get_density(N = args.Disks)
-        print (f'sigma = {sigma}, eta = {eta}')
-        configurations = [direct_disks( sigma    = sigma,
-                                        N        = args.Disks,
-                                        d        = args.d,
-                                        geometry = geometry)[:,0] for _ in range(args.N)]
-        positions      = reshape(configurations, args.N*args.Disks)
-        hist,bin_edges = histogram(positions,
-                                   bins    = args.bins,
-                                   density = True)
+        try:
+            geometry = GeometryFactory(periodic = args.periodic, L = sides, sigma = sigma, d = args.d)
+            eta = geometry.get_density(N = args.Disks)
+            print (f'sigma = {sigma}, eta = {eta}')
+            configurations = [direct_disks(sigma=sigma,N=args.Disks,d=args.d,geometry=geometry,NTrials=args.NTrials)[:,0]
+                              for _ in range(args.N)]
+            positions = np.reshape(configurations, args.N*args.Disks)
+            hist,bin_edges = np.histogram(positions, bins = args.bins, density = True)
+            xs = [0.5*(bin_edges[i] + bin_edges[i+1]) for i in range(len(bin_edges)-1)]
+            ax1.plot(xs, hist,label = fr'$\sigma=${sigma}, $\eta=$ {eta:.3}')
+            upper_limit = max(max(hist),upper_limit)
+        except RuntimeError as e:
+            print (e)
 
-        plot([0.5*(bin_edges[i] + bin_edges[i+1]) for i in range(len(bin_edges)-1)], hist,
-             label = fr'$\sigma=${sigma}, $\eta=$ {eta:.3}')
+    ax1.set_title(fr'{args.N:,} Trials, {args.Disks} Disks {geometry.get_description()}')
+    ax1.legend()
+    ax1.set_ylim([0,upper_limit])
+    ax1.set_xlabel('Position')
+    ax1.set_ylabel('Frequency')
 
-        upper_limit = max(max(hist),upper_limit)
-
-    title(fr'{args.N:,} Trials, {args.Disks} Disks {geometry.get_description()}')
-    legend()
-    ylim([0,upper_limit])
-    xlabel('Position')
-    ylabel('Frequency')
-    savefig(get_plot_file_name(args.plot))
+    fig.savefig(get_file_name(args.out))
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
 
     if args.show:
         show()
