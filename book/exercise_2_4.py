1e1e279997530b7b86abfa86f904d69223b54b7c
Simon Crase <simon@greenweaves.nz>
2025-06-17 11:13:38 +1200

    #95 Renamed source file and deleted old file

--- book/exercise_2_4.py
+++ book/exercise_2_4.py
@@ -1,174 +1,53 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2022-2025 Simon Crase
+#   Copyright (C) 2025 Simon Crase
 
-# This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 '''
     Exercise 2-4. Sinai's system of two large sphere in a box. Show histogram of positions.
 '''
 
 from argparse import ArgumentParser
-from os.path import basename, splitext, join
+from os.path import basename, join, splitext
 from time import time
+import numpy as np
+from numpy.testing import assert_allclose
 from matplotlib import rc
 from matplotlib.pyplot import figure, show
-import numpy as np
 
-class HardDisk:
-    '''Keeps track of position and velocity of disk'''
-    def __init__(self,x,y,u,v,sigma):
-        self.x = x
-        self.y = y
-        self.u = u
-        self.v = v
-        self.sigma = sigma
+def parse_arguments():
+    '''Parse command line arguments'''
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
+    parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    parser.add_argument('--L', type = float, default = 1.0, help = 'Length of box')
+    parser.add_argument('--sigma', type = float, default = 0.365, help = 'Radius of sphere')
+    parser.add_argument('--N', type = int, default = 1000, help = 'Number of steps to evolve configuration')
+    parser.add_argument('--DeltaT', type = float, default = 1.0, help = 'For sampling')
+    parser.add_argument('--freq', type = int, default = 250, help = 'For reporting')
+    parser.add_argument('--action', default = 'run',choices=['first','run'])
+    parser.add_argument('--m', type = int, default = 100, help = 'Number of bins for histogram')
+    parser.add_argument('--V', type = float, default = 1.0, help = 'Velocity')
+    args = parser.parse_args()
+    if 0.25*args.L < args.sigma and args.sigma < 0.5 * args.L:
+        return args
+    else:
+        parser.error('sigma should be between 0.25L and 0.5L')
 
-    def move(self,dt):
-        '''Update position, assuming constant velocity for time dt'''
-        self.x += self.u * dt
-        self.y += self.v * dt
-
-    def get_direction_cosines(self):
-        '''Find direction of centre, ralative to origin'''
-        radius = np.sqrt(self.x**2 + self.y**2)
-        assert np.allclose(2*self.sigma, radius,
-                        rtol = 1e-5,
-                        atol = 0),f'{2*self.sigma} {radius}'
-        return self.x/radius, self.y/radius
-
-class Collision:
-    '''Compute time to next collision of spheres and consequences of collision'''
-    def __init__(self,L,sigma,V):
-        self.L = L
-        self.sigma = sigma
-        self.a = V**2
-
-    def get_delta(self,disk):
-        '''Compute time to next collision of spheres'''
-        b = disk.x * disk.u + disk.y * disk.v
-        c = disk.x**2 + disk.y**2 - 4*self.sigma**2
-        delta = b**2 - self.a *c
-        if delta>0 and b<0:
-            return (-b - np.sqrt(delta))/self.a
-        return float('inf')
-
-    '''Collide spheres. We need to reverse the radial component of velocity, while preserving theta component.'''
-    def exec(self,disk,dt,sample):
-        disk.move(dt)
-        x, y = disk.get_direction_cosines()
-        assert np.allclose(1,x**2 + y**2,
-                        rtol = 1e-5,
-                        atol = 0),f'{1} not = {x**2 + y**2}'
-        v_r = x * disk.u + y * disk.v
-        v_theta  = - y * disk.u + x * disk.v
-        v_r *= -1
-        disk.v = v_theta * x + v_r * y
-        disk.u = v_r * x     - v_theta * y
-        assert np.allclose(self.a, disk.u**2+disk.v**2,
-                        rtol = 1e-5,
-                        atol = 0),f'{self.a} not = {disk.u**2+disk.v**2}'
-        sample.synchronize(dt)
-
-class Wrap:
-    '''Compute time to next collision with Wall and consequences of collision'''
-    def __init__(self,L):
-        self.L = L
-
-    def get_delta(self,disk):
-        '''
-        Compute time to next collision with Wall.
-        Store the number of the wall that gives next collision
-        '''
-        Deltas = [self.get_one_delta(disk.x,disk.u), self.get_one_delta(disk.y,disk.v)]
-        self.axis = np.argmin(Deltas)
-        return Deltas[self.axis]
-
-    def exec(self,disk,dt,sample):
-        '''Move disk to wall, and wrap position around.'''
-        disk.move(dt)
-        if self.axis==0:
-            disk.x *= -1
-        else:
-            disk.y *= -1
-        sample.synchronize(dt)
-
-    def get_one_delta(self,x,u):
-        '''Compute time to next collision with specified Wall'''
-        if x*u > 0:
-            return (self.L-abs(x))/abs(u)
-        else:
-            return (self.L+abs(x))/abs(u)
-
-class Sample:
-    '''Keep track of Time, and sample state at regular intervals. '''
-    def __init__(self,N,dt):
-        self.N = N
-        self.n = -1
-        self.dt = dt
-        self.t_acc = 0
-        self.xs = []
-        self.ys = []
-
-    def get_delta(self,disk):
-        '''Calculate time to next sample'''
-        return self.dt - self.t_acc
-
-    def exec(self,disk,dt,sample):
-        '''Sample position'''
-        disk.move(dt)
-        self.xs.append(disk.x)
-        self.ys.append(disk.y)
-        self.t_acc = 0
-
-    def should_continue(self):
-        '''Are we there yet?'''
-        self.n += 1
-        return self.n < self.N
-
-    def synchronize(self,dt):
-        '''Accumulate time from Collisions and Wraps'''
-        self.t_acc += dt
-
-def create_disk(L,V,sigma, rng = np.random.default_rng()):
-    '''
-       Set up one disk that is in a valid position, i.e. outside reference disk,
-       but otherwise has a random position and direction.
-
-       Parameters:
-         L     Half length of box
-         V     Magnitude of velocity
-         sigma Radius of sphere
-    '''
-    x,y = 0,0
-    while x**2 + y**2 < sigma**2:
-        x,y = L * (2 * rng.random((2))-1)
-
-    u,v = 2*rng.random((2))-1
-    norm = np.sqrt(u**2 + v**2)
-    return HardDisk(x, y, u*V/norm, v*V/norm,sigma)
-
-def get_next_event(collision, wrap, sample, disk):
-    '''
-       Generator that handles evolution in time. Repeatedly determine which type of event will happen next,
-       and when it will occur (relative to current time).
-    '''
-    events = [collision, wrap, sample]
-    while sample.should_continue():
-        next_event_times = [event.get_delta(disk) for event in events]
-        first_index      = np.argmin(next_event_times)
-        yield events[first_index],next_event_times[first_index]
 
 def get_file_name(name,default_ext='png',seq=None):
     '''
@@ -190,23 +69,70 @@ def get_file_name(name,default_ext='png',seq=None):
     else:
         return qualified_name
 
-def parse_arguments():
-    parser = ArgumentParser(description = __doc__)
-    parser.add_argument('--L', type = float, default = 1.0, help = 'Half length of box')
-    parser.add_argument('--sigma', type = float, default = 0.365, help = 'Radius of sphere')
-    parser.add_argument('--N', type = int, default = 1000000, help = 'Number of steps to evolve configuration')
-    parser.add_argument('--m', type = int, default = 100, help = 'Number of bins for histogram')
-    parser.add_argument('--seed', type = int, default = None, help = 'Seed for random number generator')
-    parser.add_argument('--dt', type = float, default = 0.1, help = 'Time step for sampling')
-    parser.add_argument('--V', type = float, default = 1.0, help = 'Magnitude of velocity')
-    parser.add_argument('--show', action = 'store_true', help = 'Show plot')
-    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
-    parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
-    return parser.parse_args()
+def create_config(sigma= 0.375,L=1,d=2,corners=np.array([[0,0],[0,1],[1,1],[1,0]]),rng = np.random.default_rng(),V=1):
+    def acceptable(x):
+        for i in range(2*d):
+            if np.dot(x - corners[i,:],x - corners[i,:]) < 4*sigma**2:
+                return False
+        return True
 
-def flatten(Xss):
-    '''Convert a list of lists into a single list'''
-    return [x for Xs in Xss for x in Xs]
+    x = L*rng.random((d))
+    while not acceptable(x):
+        x = L*rng.random((d))
+
+    v = 2*rng.random((d)) - 1
+    return x,v/np.linalg.norm(v)
+
+def get_pair_time(x, x_center, v, sigma = 0.01,cutoff=1e-6):
+    '''
+    Algorithm 2.2 Pair Time. Pair collision time for two spheres
+
+    Parameters:
+        x         Centre of moving sphere
+        x_center  Centre of the other sphere
+        v         Velocity of moving sphere
+        sigma     Radius of sphere
+
+    Returns:
+        The time at which two spheres will collide: may be np.inf
+    '''
+    Delta = x - x_center
+    Upsilon = np.dot(Delta,v)**2 + np.dot(v,v)*( 4*sigma**2 - np.dot(Delta,Delta) )
+    if Upsilon > 0:
+        dt1 = - (np.dot(Delta,v) + np.sqrt(Upsilon)) / np.dot(v,v)
+        dt2 = - (np.dot(Delta,v) - np.sqrt(Upsilon)) / np.dot(v,v)
+        if dt1 > cutoff and dt2 > cutoff: return min(dt1,dt2)
+        if dt1 > cutoff and dt2 < 0: return dt1
+        if dt1 < 0 and dt2 > cutoff: return dt2
+    return float('inf')
+
+def collide_pair(x, x_center, v):
+    '''
+        Algorithm 2.3 Pair collision
+
+        Parameters:
+            x          Centre of moving sphere
+            x_center   Centre of stationary sphere
+            v          Velocity of moving sphere
+
+        Returns:
+           Velocities after collision
+        '''
+    Delta_x = x - x_center
+    e_hat_perp = Delta_x/np.linalg.norm(Delta_x)
+    Delta_v_perp = np.dot(v,e_hat_perp)
+    return v - 2* Delta_v_perp*e_hat_perp
+
+def box_it(x,L=1,d=2):
+    '''
+    Restrict x to be within box defined by L
+    '''
+    for k in range(d):
+        while x[k] > args.L:
+            x[k] -= args.L
+        while x[k] < 0:
+            x[k] += args.L
+    return x
 
 if __name__=='__main__':
     rc('font',**{'family':'serif','serif':['Palatino']})
@@ -214,41 +140,89 @@ if __name__=='__main__':
     start  = time()
     args = parse_arguments()
     rng = np.random.default_rng(args.seed)
-    disk = create_disk(args.L, args.V, args.sigma)
-    sample = Sample(args.N,args.dt)
-    for event,dt in get_next_event(collision = Collision(args.L,args.sigma,args.V),
-                                   wrap      = Wrap(args.L),
-                                   sample    = sample,
-                                   disk      = disk):
-        event.exec(disk,dt,sample)
 
-    bins = np.linspace(-args.L, args.L, num = args.m)
+    corners = np.array([[0,0],[0,args.L],[args.L,args.L],[args.L,0]])
+    T = np.zeros((len(corners)+1))
+    X = np.zeros((args.N,2))
+    V = np.zeros((args.N,2))
+    x,v = create_config(args.sigma,args.L,corners=corners,rng=rng,V=args.V)
 
-    fig = figure(figsize=(12,6))
-    fig.suptitle('Sinai two-disk system')
+    match args.action:
+        case 'first':
+            X0 = np.zeros((args.N,2))
+            for i in range(args.N):
+                X0[i,:],_ = create_config(args.sigma,args.L,corners=corners,rng=rng)
+            X[0,:],V[0,:] =x,v
+            T[len(corners)] = args.DeltaT
+            for j in range(len(corners)):
+                T[j] = get_pair_time(x, corners[j,:], v, sigma = args.sigma)
 
-    # Plot 2d histogram showing time spent at each position
+            j = np.argmin(T)
+            x += (T[j] * v)
+            if j < len(corners):
+                assert_allclose(2*args.sigma, np.sqrt(np.dot(x- corners[j,:],x- corners[j,:])))
+                v = collide_pair(x, corners[j,:], v)
 
-    ax1 = fig.add_subplot(1,2,1)
-    ax1.axis('scaled')
-    h,_,_,mappable = ax1.hist2d(sample.xs, sample.ys, bins = [bins,bins], density = True)
-    fig.colorbar(mappable)
-    ax1.set_title(fr'L={args.L}, $\sigma=${args.sigma}, N={args.N:,d}, dt={args.dt}, V={args.V}')
-    ax1.set_xlabel('X')
-    ax1.set_ylabel('Y')
+            fig = figure(figsize=(8,12))
+            ax1 = fig.add_subplot(1,1,1)
+            ax1.scatter(X0[:,0],X0[:,1],label='background',s=1)
+            ax1.scatter(corners[:,0], corners[:,1],label='corners',marker='+')
+            ax1.scatter(X[0,0],X[0,1],label='start')
+            ax1.arrow(X[0,0],X[0,1],T[j] *V[0,0],T[j] *V[0,1],label='v')
+            ax1.scatter(x[0],x[1],label='collision',marker='x')
+            ax1.arrow(x[0],x[1],T[j] *v[0],T[j] *v[1],label='v')
+            ax1.legend()
 
-    # Show distribution of frequencies
+        case 'run':
+            X[0,:],V[0,:] =x,v
+            for i in range(1,args.N):
+                t = args.DeltaT * i
+                T[len(corners)] = args.DeltaT# + t
+                if i%args.freq == 0:
+                    print (f'Epoch={i:,}, t={t}')
+                sampled = False
+                # Iterate through a sequence of collisions until
+                # we reach a time step so we can sample
+                while not sampled:
+                    for j in range(len(corners)):
+                        T[j] = get_pair_time(x, corners[j,:], v, sigma = args.sigma)
 
-    ax2 = fig.add_subplot(1,2,2)
-    ax2.hist (flatten(h),
-          density = True,
-          color = 'xkcd:blue')
-    ax2.set_title(fr'Projected Densities $\eta=${np.pi*args.sigma**2/(4*args.L**2):.3f}')
-    ax2.set_xlabel('Density')
-    ax2.set_ylabel('Frequency')
-    ax2.grid(True)
+                    j = np.argmin(T)
+                    x += T[j] * v
+                    if j < len(corners):
+                        # assert_allclose(2*args.sigma, np.sqrt(np.dot(x- corners[j,:],x- corners[j,:])))
+                        v = collide_pair(x, corners[j,:], v)
+                        T[len(corners)] -= T[j]
+                    else:
+                        x = box_it(x,L=args.L)
+                        X[i,:],V[i,:] =x,v
+                        sampled = True
 
-    fig.savefig(get_file_name(args.out))
+            bins = np.linspace(0, args.L, num = args.m)
+            fig = figure(figsize=(8,12))
+            fig.suptitle(fr'L={args.L}, $\sigma=${args.sigma}, N={args.N:,d}, $\Delta T=${args.DeltaT}, V={args.V}')
+
+            ax1 = fig.add_subplot(2,2,1)
+            ax1.scatter(X[:,0],X[:,1],s=1)
+
+            ax2 = fig.add_subplot(2,2,2)
+            ax2.axis('scaled')
+            h,_,_,mappable = ax2.hist2d(X[:,0],X[:,1], bins = [bins,bins], density = True)
+            fig.colorbar(mappable)
+
+            ax2.set_xlabel('X')
+            ax2.set_ylabel('Y')
+
+            # Show distribution of frequencies
+
+            ax3 = fig.add_subplot(2,2,3)
+            ax3.hist (h.ravel(), density = True, color = 'xkcd:blue')
+            ax3.set_title(fr'Projected Densities $\eta=${np.pi*args.sigma**2/(4*args.L**2):.3f}')
+            ax3.set_xlabel('Density')
+            ax3.set_ylabel('Frequency')
+            ax3.grid(True)
+
+            fig.savefig(get_file_name(args.out))
 
     elapsed = time() - start
     minutes = int(elapsed/60)
