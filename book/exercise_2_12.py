efafdfb686a1ee1b524e1b0bb6c177990d850a46
Simon Crase <simon@greenweaves.nz>
2025-06-21 09:02:05 +1200

    Started #97

---
+++ book/exercise_2_12.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+#   Copyright (C) 2025 Simon Crase
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+''' Template for Python programs'''
+
+from argparse import ArgumentParser
+from os.path import basename, join, splitext
+from time import time
+import numpy as np
+from matplotlib import rc
+from matplotlib.pyplot import figure, show
+
+
+def parse_arguments():
+    '''Parse command line arguments'''
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
+    parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    return parser.parse_args()
+
+def get_piston_patricles(N,betaP=1,rng = np.random.default_rng()):
+    Upsilon = rng.random()
+    alpha = np.zeros(N)
+    for k in range(N):
+        alpha[k] = rng.random()
+        Upsilon *= rng.random()
+    L = - np.log(Upsilon)/betaP
+    return L,alpha * L
+
+def get_file_name(name,default_ext='png',seq=None):
+    '''
+    Used to create file names
+
+    Parameters:
+        name          Basis for file name
+        default_ext   Extension if non specified
+        seq           Used if there are multiple files
+    '''
+    base,ext = splitext(name)
+    if len(ext) == 0:
+        ext = default_ext
+    if seq != None:
+        base = f'{base}{seq}'
+    qualified_name = f'{base}.{ext}'
+    if ext == 'png':
+        return join(args.figs,qualified_name)
+    else:
+        return qualified_name
+
+if __name__=='__main__':
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    start  = time()
+    args = parse_arguments()
+    rng = np.random.default_rng(args.seed)
+    fig = figure(figsize=(12,12))
+
+    L = np.zeros((6))
+    X = np.zeros((6))
+    for N in range(6):
+        L[N],alpha = get_piston_patricles(N,betaP=1,rng = rng)
+        X[N] = alpha.sum()
+    ax1 = fig.add_subplot(1,1,1)
+    ax1.plot(list(range(6)),L)
+    ax1.plot(list(range(6)),X)
+    fig.savefig(get_file_name(args.out))
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
