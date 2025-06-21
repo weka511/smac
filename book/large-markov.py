0cb6398be6551eb26798b1e1da1c08f8b5a77ac4
Simon Crase <simon@greenweaves.nz>
2025-06-22 08:25:21 +1200

    #98 Started documening and converting to use numpy

--- book/large-markov.py
+++ book/large-markov.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2015 Greenweaves Software Pty Ltd
+# Copyright (C) 2015-2025 Greenweaves Software Pty Ltd
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,49 +15,104 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-import markovdiscretepebble as mdp
+'''Exercise 1.4 '''
 
+from argparse import ArgumentParser
+from os.path import basename, join, splitext
+from time import time
+import numpy as np
+from matplotlib import rc
+from matplotlib.pyplot import figure, show
+from markovdiscretepebble import step, markov_discrete_pebble, neighbour_table
 
-def explore_space(m,n,max_iterations):
+def parse_arguments():
+    '''Parse command line arguments'''
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
+    parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    parser.add_argument('--N', default=100000, type=int)
+    parser.add_argument('--m', default=200, type=int)
+    parser.add_argument('--n', default=100, type=int)
+    return parser.parse_args()
+
+def explore_space(m,n,max_iterations,rng=np.random.default_rng()):
     def getK(i,j):
         def helper(j,values):
-            if j<1:
+            if j < 1:
                 return values[0]
-            elif j<n-1:
+            elif j < n-1:
                 return values[1]
             else:
                 return values[2]
-        if i<1:
+        if i < 1:
             return helper(j,[1,2,3])
-        elif i<m-1:
+        elif i < m-1:
             return helper(j,[4,5,6])
         else:
             return helper(j,[7,8,9])
 
-    counts=[]
-    for i in range(m*n):
-        counts.append(0)
+    Counts = np.zeros((m*n))
 
-    i=0
-    j=0
-    iteration=0
-    while iteration<max_iterations:
-        k=getK(i,j)
-        k_new=mdp.markov_discrete_pebble(k,mdp.neighbour_table)
- #       print i,j,k,k_new
-        (i_step,j_step)=mdp.step(k,k_new)
-        i+=i_step
-        j+=j_step
-        counts[n*i+j]+=1
-        iteration+=1
+    i = 0
+    j = 0
+    for iteration in range(max_iterations):
+        k = getK(i,j)
+        k_new = markov_discrete_pebble(k,neighbour_table,rng=rng)
+        (i_step,j_step) = step(k,k_new)
+        i += i_step
+        j += j_step
+        Counts[n*i+j] += 1
 
-    mean=1.0/(m*n)
-    sum_sq=0.0
+    mean = 1.0/(m*n)
+    sum_sq = 0.0
 
-    for i in range(m*n):
-        diff=counts[i]/float(max_iterations)-mean
-        sum_sq+=(diff*diff)
+    Diffs = Counts/float(max_iterations) - mean
+    sum_sq =(Diffs**2).sum()
 
-    print sum_sq
+    return Counts, sum_sq, mean
 
-explore_space(200,100,100000)
+def get_file_name(name,default_ext='png',seq=None):
+    '''
+    Used to create file names
+
+    Parameters:
+        name          Basis for file name
+        default_ext   Extension if non specified
+        seq           Used if there are multiple files
+    '''
+    base,ext = splitext(name)
+    if len(ext) == 0:
+        ext = default_ext
+    if seq != None:
+        base = f'{base}{seq}'
+    qualified_name = f'{base}.{ext}'
+    if ext == 'png':
+        return join(args.figs,qualified_name)
+    else:
+        return qualified_name
+
+
+if __name__=='__main__':
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    start  = time()
+    args = parse_arguments()
+    rng = np.random.default_rng(args.seed)
+
+    Counts, sum_sq, mean = explore_space(args.m,args.n,args.N,rng=rng)
+
+    fig = figure(figsize=(12,12))
+    ax1 = fig.add_subplot(1,1,1)
+    ax1.hist(Counts)
+    ax1.set_title(f'N={args.N:,}: sum_sq={sum_sq:.3}')
+    fig.savefig(get_file_name(args.out))
+
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
