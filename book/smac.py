2bb4273b00b347dc534e1e831e102426ac96e49b
Simon Crase <simon@greenweaves.nz>
2025-06-28 18:55:52 +1200

    #101 Tidied up

--- book/smac.py
+++ book/smac.py
@@ -16,28 +16,27 @@
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
-Useful functions: BoxMuller, CircleThrowing, and SphereGenerator
+    Useful functions from Chapter 1: BoxMuller, CircleThrowing, and SphereGenerator
 '''
 
-from math              import log, pi, sqrt
+import numpy as np
 from matplotlib.pyplot import figure, hist, show, subplot, suptitle, title, xlabel, ylabel
-from random            import random
 
 class BoxMuller:
     '''Algorithm 1.19'''
-    def __init__(self,sigma=1.0):
-        self.even       = True
+    def __init__(self,sigma=1.0,rng = np.random.default_rng()):
+        self.even = True
         self.next_value = 0
-        self.sigma      = sigma
+        self.sigma = sigma
+        self.rng = rng
 
     def box_muller(self):
         upsilon1 = 2
         while upsilon1==0 or upsilon1>1:
-            x        = 2*random()-1
-            y        = 2*random()-1
+            x,y = 2*self.rng.random((2))-1
             upsilon1 = x**2 + y**2
-        upsilon   = - log(upsilon1)
-        upsilon2  = self.sigma * sqrt(2*upsilon/upsilon1)
+        upsilon = - np.log(upsilon1)
+        upsilon2 = self.sigma * np.sqrt(2*upsilon/upsilon1)
         return (x*upsilon2,y*upsilon2)
 
     def gauss(self,):
@@ -50,46 +49,46 @@ class BoxMuller:
 
 class CircleThrowing:
     '''Algorithm 1.18'''
-    def __init__(self,sigma):
+    def __init__(self,sigma,rng = np.random.default_rng()):
         self.sigma = sigma
+        self.rng = rng
 
     def gauss(self):
-        phi = random()*2*pi
-        while phi==2*pi:
-            phi = random()*2*pi
-        rr = random()
+        phi = random()*2*np.pi
+        while phi==2*np.pi:
+            phi = random()*2*np.pi
+        rr = self.rng.random()
         while rr==0:
-            rr = random()
-        upsilon = -log(rr)
-        r       = self.sigma*sqrt(2*upsilon)
-        x       = r*cos(phi)
-        y       = r*sin(phi)
+            rr = self.rng.random()
+        upsilon = -np.log(rr)
+        r = self.sigma*np.sqrt(2*upsilon)
+        x = r*cos(phi)
+        y = r*sin(phi)
         return (x,y)
 
 class SphereGenerator:
     '''Algorithm 1.21 and 1.22'''
-    def __init__(self,d,n,
-                 sigma = 1.0,
-                 gauss = BoxMuller(1.0)):
-        self.d     = d
-        self.n     = n
+    def __init__(self,d,n, sigma = 1.0, gauss = BoxMuller(1.0),rng = np.random.default_rng()):
+        self.d = d
+        self.n = n
         self.gauss = gauss
+        self.rng = rng
 
     def root_sigma(self,xs):
-        return sqrt(sum(sum(el*el for el in x) for x in xs))
+        return np.sqrt(sum(sum(el*el for el in x) for x in xs))
 
 
     def direct_sphere(self):
         '''Algorithm 1.21'''
-        xs        = [tuple([self.gauss.gauss() for j in range(self.d)]) for i in range(self.n)]
-        upsilon   = random()**(1.0/(self.d*self.n))
+        xs = [tuple([self.gauss.gauss() for j in range(self.d)]) for i in range(self.n)]
+        upsilon = rng.random()**(1.0/(self.d*self.n))
         RootSigma = self.root_sigma(xs)
         return [tuple(el*upsilon/RootSigma for el in x) for x in xs]
 
     def direct_surface(self):
         '''Algorithm 1.22'''
-        sigma = 1.0/sqrt(self.d)
-        xs    = [gauss.gauss() for k in range(self.d)]
+        sigma = 1.0/np.sqrt(self.d)
+        xs = [gauss.gauss() for k in range(self.d)]
         Sigma = sum(x*x for x in xs)
         return [x/Sigma for x in xs]
 
@@ -107,29 +106,27 @@ class SphereGenerator:
                 point = []
             return result
         else:
-            raise ValueError("Dimension {0} does not divide {1} length exactly".format(n,len(xs)))
+            raise ValueError('Dimension {0} does not divide {1} length exactly'.format(n,len(xs)))
 
-if __name__=="__main__":
-
-    N        = 1000000
-    sg       = SphereGenerator(3,N)
+if __name__=='__main__':
+    rng = np.random.default_rng()
+    N = 1000000
+    sg = SphereGenerator(3,N,rng=rng)
     energies = [x*x+y*y+z*z for (x,y,z) in sg.direct_sphere()]
 
     figure(figsize = (12,6))
     subplot(1,2,1)
-    hist(energies,bins=500)
-    title("Bolzmann Histogram")
-    xlabel("Frequency")
-    ylabel("Energy")
+    hist(energies,bins='sqrt')
+    title('Bolzmann Histogram')
+    xlabel('Frequency')
+    ylabel('Energy')
 
-    gauss = BoxMuller()
+    gauss = BoxMuller(rng=rng)
     subplot(1,2,2)
-    hist([gauss.gauss() for _ in range(1000000)],
-         bins    = 200,
-         density = True)
-    title("Gaussian Histogram")
-    xlabel("Value")
-    ylabel("Frequency")
+    hist([gauss.gauss() for _ in range(1000000)], bins = 'sqrt', density = True)
+    title('Gaussian Histogram')
+    xlabel('Value')
+    ylabel('Frequency')
 
     suptitle('SMAC tests')
     show()
