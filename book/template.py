1a05c47419f517455054c7b2c5ec1b5c948e00b2
Simon Crase <simon@greenweaves.nz>
2025-07-01 08:37:07 +1200

    #102 Added parameter --N

--- book/template.py
+++ book/template.py
@@ -32,6 +32,7 @@ def parse_arguments():
     parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
     parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
     parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    parser.add_argument('-N', '--N', type=int, default=100000, help='Number of iterations')
     return parser.parse_args()
 
 
