413ae57a2d4b547f13bcda5e8e32697e6010c43e
Simon Crase <simon@greenweaves.nz>
2025-07-03 16:19:21 +1200

    #105 Converted to standard form

--- book/levy-convolution.py
+++ book/levy-convolution.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2015,2018 Greenweaves Software Pty Ltd
+# Copyright (C) 2015-2025 Greenweaves Software Pty Ltd
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,12 +15,60 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
-import math,matplotlib.pyplot as plt
+'''Algorithm 1.32 levy convolution'''
+
+from argparse import ArgumentParser
+from os.path import basename, join, splitext
+from time import time
+import numpy as np
+from matplotlib import rc
+from matplotlib.pyplot import figure, show
+
+
+def parse_arguments():
+    '''Parse command line arguments'''
+    parser = ArgumentParser(__doc__)
+    parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
+    parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
+    parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
+    parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
+    parser.add_argument('-N', '--N', type=int, default=10, help='Number of iterations')
+    return parser.parse_args()
+
+
+def get_file_name(name,default_ext='png',seq=None):
+    '''
+    Used to create file names
+
+    Parameters:
+        name          Basis for file name
+        default_ext   Extension if non specified
+        seq           Used if there are multiple files
+    '''
+    base,ext = splitext(name)
+    if len(ext) == 0:
+        ext = default_ext
+    if seq != None:
+        base = f'{base}{seq}'
+    qualified_name = f'{base}.{ext}'
+    if ext == 'png':
+        return join(args.figs,qualified_name)
+    else:
+        return qualified_name
+
 
 def levy(pi,A_Plus=1.25,alpha=1.25):
-    x0,_  = pi[0]
-    xK,_  = pi[-1]
-    K     = len(pi)
+    '''
+    Algorithm 1.32 levy convolution
+    Parameters:
+        pi
+        A_Plus
+        alpha
+    '''
+
+    x0,_ = pi[0]
+    xK,_ = pi[-1]
+    K    = len(pi)
     Delta = (xK-x0)/(K-1)
     for k in range(K):
         x = x0 + (K + k) * Delta
@@ -35,8 +83,23 @@ def levy(pi,A_Plus=1.25,alpha=1.25):
     return [(x,p/norm) for (x,p) in pi_dash if x>= x0 and x0 <= xK]
 
 if __name__=='__main__':
-    pi=[(x/10,0.1) for x in range(11)]
-    for i in range(10):
-        pi=levy(pi)
-        plt.plot(pi)
-    plt.show()
+    rc('font',**{'family':'serif','serif':['Palatino']})
+    rc('text', usetex=True)
+    start  = time()
+    args = parse_arguments()
+    rng = np.random.default_rng(args.seed)
+    fig = figure(figsize=(12,12))
+    ax1 = fig.add_subplot(1,1,1)
+    pi = [(x/10,0.1) for x in range(11)]
+    for i in range(args.N):
+        pi = levy(pi)
+        ax1.plot(pi)
+
+    fig.savefig(get_file_name(args.out))
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
