f042eddb1ffa8df2974a55ab053f719bd4ffbbee
Simon Crase <simon@greenweaves.nz>
2025-07-13 21:06:59 +1200

    #106 Added X and Vx

--- book/ex2_3/analyze.py
+++ book/ex2_3/analyze.py
@@ -31,6 +31,8 @@ def parse_arguments():
     parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
     parser.add_argument('-o', '--out', default = basename(splitext(__file__)[0]),help='Name of output file')
     parser.add_argument('--figs', default = './figs', help = 'Name of folder where plots are to be stored')
+    parser.add_argument('--samples', default = 'samples.csv', help = 'Name of folder where plots are to be stored')
+    parser.add_argument('--path', default = r'C:\cygwin64\home\Weka\smac\book\ex2_3', help = 'Name of folder where plots are to be stored')
     parser.add_argument('--show', action = 'store_true', help   = 'Show plot')
     parser.add_argument('-N', '--N', type=int, default=100000, help='Number of iterations')
     return parser.parse_args()
@@ -54,25 +56,38 @@ def get_file_name(name,default_ext='png',figs='./figs',seq=None):
     qualified_name = f'{base}.{ext}'
     return join(figs,qualified_name) if ext == 'png' else qualified_name
 
+def get_line_count(file_name):
+    '''
+    Count lines in file
+    snarfed from https://stackoverflow.com/questions/845058/how-to-get-the-line-count-of-a-large-file-cheaply-in-python
+    '''
+    return sum(1 for _ in open(file_name))
+
+def read_data(file_name):
+    '''
+    Extract X, V, T amd E from file
+    '''
+    count = get_line_count(file_name)
+    T = np.empty((count))
+    E = np.empty((count))
+    X = np.empty((count,3))
+    V = np.empty((count,3))
+    for i,line in enumerate(open(file_name)):
+        fields = [float(f) for f in line.strip().split(',')]
+        T[i] = fields[0]
+        X[i,:] = np.array([fields[j] for j in range(1,4)])
+        V[i,:] = np.array([fields[j] for j in range(4,7)])
+        E[i] = (V[i,:]**2).sum()
+    return T,E,X,V
+
 if __name__=='__main__':
     rc('font',**{'family':'serif','serif':['Palatino']})
     rc('text', usetex=True)
     start  = time()
     args = parse_arguments()
     rng = np.random.default_rng(args.seed)
-    file_name = r'C:\cygwin64\home\Weka\smac\book\ex2_3\samples.csv'
-    count = 0
-    for line in open(file_name):
-        count += 1
 
-    E = np.empty((count))
-    X = np.empty((count,3))
-    V = np.empty((count,3))
-    for i,line in enumerate(open(file_name)):
-        fields = [float(f) for f in line.strip().split(',')]
-        X[i,:] = np.array([fields[j] for j in range(1,4)])
-        V[i,:] = np.array([fields[j] for j in range(4,7)])
-        E[i] = (V[i,:]**2).sum()
+    T,E,X,V = read_data(join(args.path,args.samples))
 
     fig = figure(figsize=(12,12))
     ax1 = fig.add_subplot(2,2,1)
