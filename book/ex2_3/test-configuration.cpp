1ae15cf1e18c53f1d9d607cead70c726dd685282
Weka <simon@greenweaves.nz>
2025-07-15 14:41:44 +1200

    #106 wrote tests for creating configuration

--- book/ex2_3/test-configuration.cpp
+++ book/ex2_3/test-configuration.cpp
@@ -14,19 +14,26 @@
  * You should have received a copy of the GNU General Public License
  * along with this software.  If not, see <http://www.gnu.org/licenses/>
  *
- * This file contains tests for the Particle class
+ * This file contains tests for the Configuration class
  */
  
 
 #include "catch.hpp"
 #include "configuration.hpp"
 
+using namespace std;
+
+double create_bad_configuration() {
+	Configuration configuration(250, 1.0, 1.0, 1.0/32.0, 10);
+	return 0;
+}
+
 TEST_CASE( "Configuration Tests", "[config]" ) {
 	
 	SECTION("Test distance"){
-		Configuration atus(10, 1.0, 1.0, 1.0/32.0, 10);
+		Configuration configuration(10, 1.0, 1.0, 1.0/32.0, 10);
+		REQUIRE(10==configuration.size());
+		REQUIRE_THROWS_AS(create_bad_configuration(), runtime_error);
 	}
 	
-
-
 }
